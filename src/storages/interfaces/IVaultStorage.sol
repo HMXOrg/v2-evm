9f0b21e6ca8f754db2f988fad314c5dd962aa6cd
Kim Hera <kim.hera.20.20.20@gmail.com>
2023-02-13 17:05:05 +0700

    fix: return type

--- src/storages/interfaces/IVaultStorage.sol
+++ src/storages/interfaces/IVaultStorage.sol
@@ -3,22 +3,22 @@ pragma solidity 0.8.18;
 
 interface IVaultStorage {
   function liquidityProviderBalances(
-    address liquidityProvider,
-    address token
-  ) external view returns (uint amount);
+    address _liquidityProvider,
+    address _token
+  ) external view returns (uint _amount);
 
   function liquidityProviderTokens(
-    address liquidityProvider,
-    address token
-  ) external view returns (uint amount);
+    address _liquidityProvider,
+    address _token
+  ) external view returns (address[] memory _tokens);
 
   function traderBalances(
-    address trader,
-    address token
+    address _trader,
+    address _token
   ) external view returns (uint amount);
 
   function traderTokens(
-    address trader,
-    address token
-  ) external view returns (uint amount);
+    address _trader,
+    address _token
+  ) external view returns (address[] memory _tokens);
 }

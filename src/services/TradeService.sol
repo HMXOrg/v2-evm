ee54ce121e12d1cdbfd3b39cb7fc88b086c4d9bc
jinlong9922 <jin.long9922@gmail.com>
2023-02-13 17:38:46 +0700

    chore: add TradeService

---
+++ src/services/TradeService.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+// interfaces
+import { ITradeService } from "./interfaces/ITradeService.sol";
+
+contract TradeService is ITradeService {}

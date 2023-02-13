b93be9fb82371983992ff4152046a5b0a3357e46
ct <chentang88158@gmail.com>
2023-02-13 16:17:33 +0700

    feat: move struct to interface

--- src/storages/ConfigStorage.sol
+++ src/storages/ConfigStorage.sol
@@ -7,52 +7,6 @@ import { IConfigStorage } from "./interfaces/IConfigStorage.sol";
 /// @title ConfigStorage
 /// @notice storage contract to keep configs
 contract ConfigStorage is IConfigStorage {
-  /// @notice asset class
-  enum AssetClass {
-    Crypto
-  }
-
-  /// @notice perp liquidity provider token config
-  struct PLPTokenConfig {
-    uint8 decimals;
-    uint256 targetWeight;
-    uint256 bufferLiquidity; // liquidity reserved for swapping, decimal is depends on token
-    uint256 maxWeightDiff;
-    bool isStableCoin; // token is stablecoin
-    bool accepted; // accepted to provide liquidity
-  }
-
-  /// @notice collateral token config
-  struct CollateralTokenConfig {
-    uint8 decimals;
-    uint256 collateralFactor; // token reliability factor to calculate buying power, 1e18 = 100%
-    bool isStableCoin; // token is stablecoin
-    bool accepted; // accepted to deposit as collateral
-    address settleStrategy; // determine token will be settled for NON PLP collateral, e.g. aUSDC redeemed as USDC
-  }
-
-  struct MarketConfig {
-    bytes32 assetId; // pyth network asset id
-    uint256 maxProfitRate; // maximum profit that trader could take per position
-    uint256 longMaxOpenInterestUSDE30; // maximum to open long position
-    uint256 shortMaxOpenInterestUSDE30; // maximum to open short position
-    uint256 maxLeverage; // maximum leverage that trader could open position
-    uint256 minLeverage; // minimum leverage that trader could open position
-    uint256 initialMarginFraction; // IMF
-    uint256 maintenanceMarginFraction; // MMF
-    uint256 increasePositionFeeRate; // fee rate to increase position
-    uint256 decreasePositionFeeRate; // fee rate to decrease position
-    uint256 maxFundingRate; // maximum funding rate
-    uint256 priceConfidentThreshold; // pyth price confidential treshold
-    AssetClass assetClass;
-    bool allowIncreasePosition; // allow trader to increase position
-    bool active; // if active = false, means this market is delisted
-  }
-
-  struct AssetClassConfig {
-    uint256 baseBorrowingRate;
-  }
-
   // GLOBAL Parameter Configs
   // Liquidity
   uint256 depositFeeRate; // PLP deposit fee rate
@@ -76,11 +30,9 @@ contract ConfigStorage is IConfigStorage {
 
   MarketConfig[] marketConfigs;
 
-  mapping(address => PLPTokenConfig) plpTokenConfigs; // token => config
-  mapping(address => CollateralTokenConfig) colalteralTokenConfigs; // token => config
+  mapping(address => IConfigStorage.PLPTokenConfig) plpTokenConfigs; // token => config
+  mapping(address => IConfigStorage.CollateralTokenConfig) colalteralTokenConfigs; // token => config
 
   mapping(address => bool) allowedLiquidators; // allowed contract to execute liquidation service
   mapping(address => mapping(address => bool)) serviceExecutors; // to allowed executor for service layer
-
-  
 }
--- src/storages/interfaces/IConfigStorage.sol
+++ src/storages/interfaces/IConfigStorage.sol
@@ -1,4 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-interface IConfigStorage {}
+interface IConfigStorage {
+  /// @notice asset class
+  enum AssetClass {
+    Crypto
+  }
+
+  /// @notice perp liquidity provider token config
+  struct PLPTokenConfig {
+    uint8 decimals;
+    uint256 targetWeight;
+    uint256 bufferLiquidity; // liquidity reserved for swapping, decimal is depends on token
+    uint256 maxWeightDiff;
+    bool isStableCoin; // token is stablecoin
+    bool accepted; // accepted to provide liquidity
+  }
+
+  /// @notice collateral token config
+  struct CollateralTokenConfig {
+    uint8 decimals;
+    uint256 collateralFactor; // token reliability factor to calculate buying power, 1e18 = 100%
+    bool isStableCoin; // token is stablecoin
+    bool accepted; // accepted to deposit as collateral
+    address settleStrategy; // determine token will be settled for NON PLP collateral, e.g. aUSDC redeemed as USDC
+  }
+
+  struct MarketConfig {
+    bytes32 assetId; // pyth network asset id
+    uint256 maxProfitRate; // maximum profit that trader could take per position
+    uint256 longMaxOpenInterestUSDE30; // maximum to open long position
+    uint256 shortMaxOpenInterestUSDE30; // maximum to open short position
+    uint256 maxLeverage; // maximum leverage that trader could open position
+    uint256 minLeverage; // minimum leverage that trader could open position
+    uint256 initialMarginFraction; // IMF
+    uint256 maintenanceMarginFraction; // MMF
+    uint256 increasePositionFeeRate; // fee rate to increase position
+    uint256 decreasePositionFeeRate; // fee rate to decrease position
+    uint256 maxFundingRate; // maximum funding rate
+    uint256 priceConfidentThreshold; // pyth price confidential treshold
+    AssetClass assetClass;
+    bool allowIncreasePosition; // allow trader to increase position
+    bool active; // if active = false, means this market is delisted
+  }
+
+  struct AssetClassConfig {
+    uint256 baseBorrowingRate;
+  }
+}

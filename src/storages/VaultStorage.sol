47eb5deaab29bc645590c97161fc568317fd1be3
Kim Hera <kim.hera.20.20.20@gmail.com>
2023-02-13 15:02:43 +0700

    feat: define states

--- src/storages/VaultStorage.sol
+++ src/storages/VaultStorage.sol
@@ -6,6 +6,71 @@ import { IVaultStorage } from "./interfaces/IVaultStorage.sol";
 
 /// @title VaultStorage
 /// @notice storage contract to do accounting for token, and also hold physical tokens
-contract VaultStorage is IVaultStorage {
+abstract contract VaultStorage is IVaultStorage {
+  // liquidity provider address => token => amount
+  mapping(address => mapping(address => uint256))
+    public liquidityProviderBalances;
+  mapping(address => address[]) public liquidityProviderTokens;
 
+  // trader address => token => amount
+  mapping(address => mapping(address => uint256)) public traderBalances;
+  mapping(address => address[]) public traderTokens;
+
+  // TODO: move to service
+  function incrementLPBalance(
+    address liquidityProviderAddress,
+    address token,
+    uint256 amount
+  ) external {
+    uint oldBalance = liquidityProviderBalances[liquidityProviderAddress][
+      token
+    ];
+    uint newBalance = oldBalance + amount;
+
+    liquidityProviderBalances[liquidityProviderAddress][token] = newBalance;
+
+    // register new token to a user
+    if (oldBalance == 0 && newBalance != 0) {
+      address[] storage liquidityProviderToken = liquidityProviderTokens[
+        liquidityProviderAddress
+      ];
+      liquidityProviderToken.push(token);
+    }
+  }
+
+  // TODO: move to service
+  function decrementLPBalance(
+    address liquidityProviderAddress,
+    address token,
+    uint256 amount
+  ) external {
+    uint oldBalance = liquidityProviderBalances[liquidityProviderAddress][
+      token
+    ];
+    if (amount > oldBalance) revert("insufficient balance");
+
+    uint newBalance = oldBalance - amount;
+    liquidityProviderBalances[liquidityProviderAddress][token] = newBalance;
+
+    // deregister token, if the use remove all of the token out
+    if (oldBalance != 0 && newBalance == 0) {
+      address[] storage liquidityProviderToken = liquidityProviderTokens[
+        liquidityProviderAddress
+      ];
+      uint256 tokenLen = liquidityProviderToken.length;
+      uint256 lastTokenIndex = tokenLen - 1;
+
+      // find and deregister the token
+      for (uint256 i; i < tokenLen; i++) {
+        if (liquidityProviderToken[i] == token) {
+          // delete the token by replacing it with the last one and then pop it from there
+          if (i != lastTokenIndex) {
+            liquidityProviderToken[i] = liquidityProviderToken[lastTokenIndex];
+          }
+          liquidityProviderToken.pop();
+          break;
+        }
+      }
+    }
+  }
 }
--- src/storages/interfaces/IVaultStorage.sol
+++ src/storages/interfaces/IVaultStorage.sol
@@ -1,4 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-interface IVaultStorage {}
+interface IVaultStorage {
+  function liquidityProviderBalances(
+    address liquidityProvider,
+    address token
+  ) external view returns (uint amount);
+
+  function liquidityProviderTokens(
+    address liquidityProvider,
+    address token
+  ) external view returns (uint amount);
+
+  function traderBalances(
+    address trader,
+    address token
+  ) external view returns (uint amount);
+
+  function traderTokens(
+    address trader,
+    address token
+  ) external view returns (uint amount);
+}

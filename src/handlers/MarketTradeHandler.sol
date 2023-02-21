5dc05ee7cf214923e100e7fc22c7ce2cf31dcf03
Kim Hera <kim.hera.20.20.20@gmail.com>
2023-02-21 12:47:48 +0700

    feat: implement market trade handler

---
+++ src/handlers/MarketTradeHandler.sol
@@ -0,0 +1,202 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { Owned } from "../base/Owned.sol";
+
+// Interfaces
+import { IPyth } from "pyth-sdk-solidity/IPyth.sol";
+import { IMarketTradeHandler } from "./interfaces/IMarketTradeHandler.sol";
+import { ITradeService } from "../services/interfaces/ITradeService.sol";
+import { IConfigStorage } from "../storages/interfaces/IConfigStorage.sol";
+import { IPerpStorage } from "../storages/interfaces/IPerpStorage.sol";
+
+contract MarketTradeHandler is Owned, ReentrancyGuard, IMarketTradeHandler {
+  // EVENTS
+  event LogSetTradeService(address indexed oldMarketTradeService, address newMarketTradeService);
+  event LogSetPyth(address indexed oldPyth, address newPyth);
+
+  // STATES
+  address public tradeService;
+  address public pyth;
+
+  constructor(address _tradeService, address _pyth) {
+    // @todo sanity check
+    tradeService = _tradeService;
+    pyth = _pyth;
+  }
+
+  /**
+   * MODIFIER
+   */
+
+  /**
+   * SETTER
+   */
+
+  function setTradeService(address _newTradeService) external onlyOwner {
+    // @todo - Sanity check
+    if (_newTradeService == address(0)) revert IMarketTradeHandler_InvalidAddress();
+    emit LogSetTradeService(address(tradeService), _newTradeService);
+    tradeService = _newTradeService;
+  }
+
+  function setPyth(address _newPyth) external onlyOwner {
+    // @todo - Sanity check
+    if (_newPyth == address(0)) revert IMarketTradeHandler_InvalidAddress();
+    emit LogSetPyth(address(pyth), _newPyth);
+    pyth = _newPyth;
+  }
+
+  /**
+   * CALCULATION
+   */
+
+  function buy(
+    address _account,
+    uint256 _subAccountId,
+    uint256 _marketIndex,
+    uint256 _buySizeE30,
+    bytes[] memory _priceData
+  ) external {
+    // Feed Price
+    IPyth(pyth).updatePriceFeeds{ value: IPyth(pyth).getUpdateFee(_priceData) }(_priceData);
+
+    // 0. Get position
+    IPerpStorage.Position memory _position = _getPosition(_account, _subAccountId, _marketIndex);
+
+    // 1. Find the `_shortDecreasingSizeE30` and `_longIncreasingSizeE30`
+    uint256 _shortDecreasingSizeE30;
+    uint256 _longIncreasingSizeE30;
+    {
+      if (_position.positionSizeE30 < 0) {
+        // If short position exists, we need to close it first
+
+        uint256 _longPositionSizeE30 = uint256(-_position.positionSizeE30);
+
+        if (_buySizeE30 > _longPositionSizeE30) {
+          // If buy size can cover the short position size,
+          // long position size should be the remaining buy size
+          _shortDecreasingSizeE30 = _longPositionSizeE30;
+          _longIncreasingSizeE30 = _buySizeE30 - _longPositionSizeE30;
+        } else {
+          // If buy size cannot cover the short position size,
+          // just simply decrease the position
+          _shortDecreasingSizeE30 = _buySizeE30;
+          // can be commented to save gas
+          // _longIncreasingSizeE30 = 0;
+        }
+      } else {
+        // If short position does not exists,
+        // just simply increase the long position
+
+        // can be commented to save gas
+        // _shortDecreasingSizeE30 = 0;
+        _longIncreasingSizeE30 = _buySizeE30;
+      }
+    }
+
+    // 2. Decrease the short position first
+    if (_shortDecreasingSizeE30 > 0) {
+      ITradeService(tradeService).decreasePosition(_account, _subAccountId, _marketIndex, _shortDecreasingSizeE30);
+    }
+
+    // 3. Then, increase the long position
+    if (_longIncreasingSizeE30 > 0) {
+      ITradeService(tradeService).increasePosition(
+        _account,
+        _subAccountId,
+        _marketIndex,
+        int256(_longIncreasingSizeE30)
+      );
+    }
+  }
+
+  function sell(
+    address _account,
+    uint256 _subAccountId,
+    uint256 _marketIndex,
+    uint256 _sellSizeE30,
+    bytes[] memory _priceData
+  ) external {
+    // Feed Price
+    IPyth(pyth).updatePriceFeeds{ value: IPyth(pyth).getUpdateFee(_priceData) }(_priceData);
+
+    // 0. Get position
+    IPerpStorage.Position memory _position = _getPosition(_account, _subAccountId, _marketIndex);
+
+    // 1. Find the `_longDecreasingSizeE30` and `_shortIncreasingSizeE30`
+    uint256 _longDecreasingSizeE30;
+    uint256 _shortIncreasingSizeE30;
+    {
+      if (_position.positionSizeE30 > 0) {
+        // If long position exists, we need to close it first
+
+        uint256 _longPositionSizeE30 = uint256(_position.positionSizeE30);
+
+        if (_sellSizeE30 > _longPositionSizeE30) {
+          // If sell size can cover the long position size,
+          // short position size should be the remaining sell size
+          _longDecreasingSizeE30 = _longPositionSizeE30;
+          _shortIncreasingSizeE30 = _sellSizeE30 - _longPositionSizeE30;
+        } else {
+          // If sell size cannot cover the short position size,
+          // just simply decrease the position
+          _longDecreasingSizeE30 = _sellSizeE30;
+          // can be commented to save gas
+          // _shortIncreasingSizeE30 = 0;
+        }
+      } else {
+        // If long position does not exists,
+        // just simply increase the short position
+
+        // can be commented to save gas
+        // _longDecreasingSizeE30 = 0;
+        _shortIncreasingSizeE30 = _sellSizeE30;
+      }
+    }
+
+    // 2. Decrease the long position first
+    if (_longDecreasingSizeE30 > 0) {
+      ITradeService(tradeService).decreasePosition(_account, _subAccountId, _marketIndex, _longDecreasingSizeE30);
+    }
+
+    // 3. Then, increase the short position
+    if (_shortIncreasingSizeE30 > 0) {
+      ITradeService(tradeService).increasePosition(
+        _account,
+        _subAccountId,
+        _marketIndex,
+        -int256(_shortIncreasingSizeE30)
+      );
+    }
+  }
+
+  /// @notice Calculate subAccount address on trader.
+  /// @dev This uses to create subAccount address combined between Primary account and SubAccount ID.
+  /// @param _primary Trader's primary wallet account.
+  /// @param _subAccountId Trader's sub account ID.
+  /// @return _subAccount Trader's sub account address used for trading.
+  function _getSubAccount(address _primary, uint256 _subAccountId) internal pure returns (address _subAccount) {
+    if (_subAccountId > 255) revert();
+    return address(uint160(_primary) ^ uint160(_subAccountId));
+  }
+
+  // @todo - add description
+  function _getPositionId(address _account, uint256 _marketIndex) internal pure returns (bytes32) {
+    return keccak256(abi.encodePacked(_account, _marketIndex));
+  }
+
+  // @todo - add description
+  function _getPosition(
+    address _account,
+    uint256 _subAccountId,
+    uint256 _marketIndex
+  ) internal view returns (IPerpStorage.Position memory) {
+    address _perpStorage = ITradeService(tradeService).perpStorage();
+    address _subAccount = _getSubAccount(_account, _subAccountId);
+    bytes32 _positionId = _getPositionId(_subAccount, _marketIndex);
+
+    return IPerpStorage(_perpStorage).getPositionById(_positionId);
+  }
+}
---
+++ src/handlers/interfaces/IMarketTradeHandler.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+interface IMarketTradeHandler {
+  // ERRORs
+  error IMarketTradeHandler_InvalidAddress();
+  error IMarketTradeHandler_PositionNotFullyClosed();
+}

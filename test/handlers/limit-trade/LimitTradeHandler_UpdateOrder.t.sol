3005ceadde5080a645cf71ef465a1452d4acedc8
Jonathan Xina <john.xina.p88@gmail.com>
2023-02-21 17:24:16 +0700

    feat: unit test update order

--- test/handlers/limit-trade/LimitTradeHandler_UpdateOrder.t.sol
+++ test/handlers/limit-trade/LimitTradeHandler_UpdateOrder.t.sol
@@ -1,91 +1,89 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.18;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
-// import { LimitTradeHandler_Base, IPerpStorage } from "./LimitTradeHandler_Base.t.sol";
-// import { ILimitTradeHandler } from "../../../src/handlers/interfaces/ILimitTradeHandler.sol";
+import { LimitTradeHandler_Base, IPerpStorage } from "./LimitTradeHandler_Base.t.sol";
+import { ILimitTradeHandler } from "../../../src/handlers/interfaces/ILimitTradeHandler.sol";
 
-// // What is this test DONE
-// // - revert
-// //   - Try updating a non existent order
-// // - success
-// //   - Try updating an order and check that it is updated correctly
+// What is this test DONE
+// - revert
+//   - Try updating a non existent order
+// - success
+//   - Try updating an order and check that it is updated correctly
 
-// contract LimitTradeHandler_UpdateOrder is LimitTradeHandler_Base {
-//   function setUp() public override {
-//     super.setUp();
-//   }
+contract LimitTradeHandler_UpdateOrder is LimitTradeHandler_Base {
+  function setUp() public override {
+    super.setUp();
+  }
 
-//   function testRevert_update_NonExistentOrder() external {
-//     vm.expectRevert(abi.encodeWithSignature("ILimitTradeHandler_NonExistentOrder()"));
-//     limitTradeHandler.updateOrder({
-//       _orderType: INCREASE,
-//       _subAccountId: 0,
-//       _orderIndex: 0,
-//       _sizeDelta: 100,
-//       _triggerPrice: 1000,
-//       _triggerAboveThreshold: true
-//     });
-//   }
+  function testRevert_update_NonExistentOrder() external {
+    vm.expectRevert(abi.encodeWithSignature("ILimitTradeHandler_NonExistentOrder()"));
+    limitTradeHandler.updateOrder({
+      _subAccountId: 0,
+      _orderIndex: 0,
+      _sizeDelta: 100,
+      _triggerPrice: 1000,
+      _triggerAboveThreshold: true,
+      _reduceOnly: false
+    });
+  }
 
-//   function testCorrectness_updateOrder() external {
-//     limitTradeHandler.createOrder{ value: 0.1 ether }({
-//       _orderType: INCREASE,
-//       _subAccountId: 0,
-//       _marketIndex: 1,
-//       _sizeDelta: 100,
-//       _triggerPrice: 1000,
-//       _triggerAboveThreshold: true,
-//       _executionFee: 0.1 ether
-//     });
+  function testCorrectness_updateOrder() external {
+    limitTradeHandler.createOrder{ value: 0.1 ether }({
+      _subAccountId: 0,
+      _marketIndex: 1,
+      _sizeDelta: 100,
+      _triggerPrice: 1000,
+      _triggerAboveThreshold: true,
+      _executionFee: 0.1 ether,
+      _reduceOnly: false
+    });
 
-//     ILimitTradeHandler.LimitOrder memory limitOrder;
-//     (
-//       ,
-//       limitOrder.account,
-//       limitOrder.subAccountId,
-//       limitOrder.marketIndex,
-//       limitOrder.sizeDelta,
-//       limitOrder.isLong,
-//       limitOrder.triggerPrice,
-//       limitOrder.triggerAboveThreshold,
-//       limitOrder.executionFee
-//     ) = limitTradeHandler.limitOrders(address(this), 0);
-//     assertEq(limitOrder.account, address(this));
-//     assertEq(limitOrder.subAccountId, 0);
-//     assertEq(limitOrder.marketIndex, 1);
-//     assertEq(limitOrder.sizeDelta, 100);
-//     assertEq(limitOrder.isLong, true);
-//     assertEq(limitOrder.triggerPrice, 1000);
-//     assertEq(limitOrder.triggerAboveThreshold, true);
-//     assertEq(limitOrder.executionFee, 0.1 ether);
+    ILimitTradeHandler.LimitOrder memory limitOrder;
+    (
+      limitOrder.account,
+      limitOrder.subAccountId,
+      limitOrder.marketIndex,
+      limitOrder.sizeDelta,
+      limitOrder.triggerPrice,
+      limitOrder.triggerAboveThreshold,
+      limitOrder.executionFee,
+      limitOrder.reduceOnly
+    ) = limitTradeHandler.limitOrders(address(this), 0);
+    assertEq(limitOrder.account, address(this));
+    assertEq(limitOrder.subAccountId, 0);
+    assertEq(limitOrder.marketIndex, 1);
+    assertEq(limitOrder.sizeDelta, 100);
+    assertEq(limitOrder.triggerPrice, 1000);
+    assertEq(limitOrder.triggerAboveThreshold, true);
+    assertEq(limitOrder.executionFee, 0.1 ether);
+    assertEq(limitOrder.reduceOnly, false);
 
-//     limitTradeHandler.updateOrder({
-//       _orderType: INCREASE,
-//       _subAccountId: 0,
-//       _orderIndex: 0,
-//       _sizeDelta: 200,
-//       _triggerPrice: 2000,
-//       _triggerAboveThreshold: true
-//     });
+    limitTradeHandler.updateOrder({
+      _subAccountId: 0,
+      _orderIndex: 0,
+      _sizeDelta: 200,
+      _triggerPrice: 2000,
+      _triggerAboveThreshold: true,
+      _reduceOnly: false
+    });
 
-//     (
-//       ,
-//       limitOrder.account,
-//       limitOrder.subAccountId,
-//       limitOrder.marketIndex,
-//       limitOrder.sizeDelta,
-//       limitOrder.isLong,
-//       limitOrder.triggerPrice,
-//       limitOrder.triggerAboveThreshold,
-//       limitOrder.executionFee
-//     ) = limitTradeHandler.limitOrders(address(this), 0);
-//     assertEq(limitOrder.account, address(this));
-//     assertEq(limitOrder.subAccountId, 0);
-//     assertEq(limitOrder.marketIndex, 1);
-//     assertEq(limitOrder.sizeDelta, 200);
-//     assertEq(limitOrder.isLong, true);
-//     assertEq(limitOrder.triggerPrice, 2000);
-//     assertEq(limitOrder.triggerAboveThreshold, true);
-//     assertEq(limitOrder.executionFee, 0.1 ether);
-//   }
-// }
+    (
+      limitOrder.account,
+      limitOrder.subAccountId,
+      limitOrder.marketIndex,
+      limitOrder.sizeDelta,
+      limitOrder.triggerPrice,
+      limitOrder.triggerAboveThreshold,
+      limitOrder.executionFee,
+      limitOrder.reduceOnly
+    ) = limitTradeHandler.limitOrders(address(this), 0);
+    assertEq(limitOrder.account, address(this));
+    assertEq(limitOrder.subAccountId, 0);
+    assertEq(limitOrder.marketIndex, 1);
+    assertEq(limitOrder.sizeDelta, 200);
+    assertEq(limitOrder.triggerPrice, 2000);
+    assertEq(limitOrder.triggerAboveThreshold, true);
+    assertEq(limitOrder.executionFee, 0.1 ether);
+    assertEq(limitOrder.reduceOnly, false);
+  }
+}

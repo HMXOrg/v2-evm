74c7369ac8bbbc0e4ae8f62f67e2946544567ea1
jinlong9922 <jin.long9922@gmail.com>
2023-03-08 10:21:16 +0700

    chore: apply approve token

--- test/integration/BaseIntTest_WithActions.i.sol
+++ test/integration/BaseIntTest_WithActions.i.sol
@@ -1,90 +1,102 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import { BaseIntTest_SetMarkets } from "./BaseIntTest_SetMarkets.i.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract BaseIntTest_WithActions is BaseIntTest_SetMarkets {
+import { BaseIntTest_SetMarkets } from "@hmx-test/integration/BaseIntTest_SetMarkets.i.sol";
+
+contract BaseIntTestWithActions is BaseIntTest_SetMarkets {
   /**
    * Liquidity
    */
 
   /// @notice Helper function to create liquidity and execute order via handler
-  /// @param _liquidityProvider liquidity provider address
-  /// @param _tokenIn liquidity token to add
-  /// @param _amountIn amount of token to provide
-  /// @param _executionFee execution fee
-  /// @param _priceData Pyth's price data
+  /// @param liquidityProvider liquidity provider address
+  /// @param tokenIn liquidity token to add
+  /// @param amountIn amount of token to provide
+  /// @param executionFee execution fee
+  /// @param priceData Pyth's price data
   function addLiquidity(
-    address _liquidityProvider,
-    address _tokenIn,
-    uint256 _amountIn,
-    uint256 _executionFee,
-    bytes[] memory _priceData
+    address liquidityProvider,
+    ERC20 tokenIn,
+    uint256 amountIn,
+    uint256 executionFee,
+    bytes[] memory priceData
   ) internal {
-    vm.prank(_liquidityProvider);
-    /// note: _minOut always 0 to make test passed
-    /// note: _shouldWrap treat as false when only GLP could be liquidity
-    liquidityHandler.createAddLiquidityOrder{ value: _executionFee }(_tokenIn, _amountIn, 0, _executionFee, false);
+    vm.startPrank(liquidityProvider);
+    tokenIn.approve(address(liquidityHandler), amountIn);
+    /// note: minOut always 0 to make test passed
+    /// note: shouldWrap treat as false when only GLP could be liquidity
+    liquidityHandler.createAddLiquidityOrder{ value: executionFee }(address(tokenIn), amountIn, 0, executionFee, false);
+    vm.stopPrank();
 
-    liquidityHandler.executeOrder(_liquidityProvider, 0, _priceData);
+    liquidityHandler.executeOrder(liquidityProvider, 0, priceData);
   }
 
   /// @notice Helper function to remove liquidity and execute order via handler
-  /// @param _liquidityProvider liquidity provider address
-  /// @param _tokenOut liquidity token to remove
-  /// @param _amountIn PLP amount to remove
-  /// @param _executionFee execution fee
-  /// @param _priceData Pyth's price data
+  /// @param liquidityProvider liquidity provider address
+  /// @param tokenOut liquidity token to remove
+  /// @param amountIn PLP amount to remove
+  /// @param executionFee execution fee
+  /// @param priceData Pyth's price data
   function removeLiquidity(
-    address _liquidityProvider,
-    address _tokenOut,
-    uint256 _amountIn,
-    uint256 _executionFee,
-    bytes[] calldata _priceData
+    address liquidityProvider,
+    ERC20 tokenOut,
+    uint256 amountIn,
+    uint256 executionFee,
+    bytes[] calldata priceData
   ) internal {
-    vm.prank(_liquidityProvider);
-    /// note: _minOut always 0 to make test passed
-    /// note: _shouldWrap treat as false when only GLP could be liquidity
-    liquidityHandler.createRemoveLiquidityOrder{ value: _executionFee }(_tokenOut, _amountIn, 0, _executionFee, false);
+    vm.startPrank(liquidityProvider);
+    tokenOut.approve(address(liquidityHandler), amountIn);
+    /// note: minOut always 0 to make test passed
+    /// note: shouldWrap treat as false when only GLP could be liquidity
+    liquidityHandler.createRemoveLiquidityOrder{ value: executionFee }(
+      address(tokenOut),
+      amountIn,
+      0,
+      executionFee,
+      false
+    );
+    vm.stopPrank();
 
-    liquidityHandler.executeOrder(_liquidityProvider, 0, _priceData);
+    liquidityHandler.executeOrder(liquidityProvider, 0, priceData);
   }
 
   /**
    * Cross Margin
    */
   /// @notice Helper function to deposit collateral via handler
-  /// @param _account Trader's address
-  /// @param _subAccountId Trader's sub-account ID
-  /// @param _collateralToken Collateral token address
-  /// @param _depositAmount amount to deposit
+  /// @param account Trader's address
+  /// @param subAccountId Trader's sub-account ID
+  /// @param collateralToken Collateral token to deposit
+  /// @param depositAmount amount to deposit
   function depositCollateral(
-    address _account,
-    uint8 _subAccountId,
-    address _collateralToken,
-    uint256 _depositAmount
+    address account,
+    uint8 subAccountId,
+    ERC20 collateralToken,
+    uint256 depositAmount
   ) internal {
-    // @todo - approve token
-    vm.prank(_account);
-    crossMarginHandler.depositCollateral(_account, _subAccountId, _collateralToken, _depositAmount);
+    vm.startPrank(account);
+    collateralToken.approve(address(crossMarginHandler), depositAmount);
+    crossMarginHandler.depositCollateral(account, subAccountId, address(collateralToken), depositAmount);
+    vm.stopPrank();
   }
 
   /// @notice Helper function to withdraw collateral via handler
-  /// @param _account Trader's address
-  /// @param _subAccountId Trader's sub-account ID
-  /// @param _collateralToken Collateral token address
-  /// @param _withdrawAmount amount to withdraw
-  /// @param _priceData Pyth's price data
+  /// @param account Trader's address
+  /// @param subAccountId Trader's sub-account ID
+  /// @param collateralToken Collateral token to withdraw
+  /// @param withdrawAmount amount to withdraw
+  /// @param priceData Pyth's price data
   function withdrawCollateral(
-    address _account,
-    uint8 _subAccountId,
-    address _collateralToken,
-    uint256 _withdrawAmount,
-    bytes[] calldata _priceData
+    address account,
+    uint8 subAccountId,
+    ERC20 collateralToken,
+    uint256 withdrawAmount,
+    bytes[] calldata priceData
   ) internal {
-    // @todo - approve
-    vm.prank(_account);
-    crossMarginHandler.withdrawCollateral(_account, _subAccountId, _collateralToken, _withdrawAmount, _priceData);
+    vm.prank(account);
+    crossMarginHandler.withdrawCollateral(account, subAccountId, address(collateralToken), withdrawAmount, priceData);
   }
 
   /**

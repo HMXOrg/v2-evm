1280906fbb4f8865dbdc0ad9d87b4bdf6ac04f3e
jinlong9922 <jin.long9922@gmail.com>
2023-03-09 13:05:34 +0700

    feat: deploy handlers

--- script/deployment/06_DeployHandlers.s.sol
+++ script/deployment/06_DeployHandlers.s.sol
@@ -3,27 +3,43 @@ pragma solidity 0.8.18;
 
 import { ConfigJsonRepo } from "@hmx-script/utils/ConfigJsonRepo.s.sol";
 
-import { Calculator } from "@hmx/contracts/Calculator.sol";
-import { FeeCalculator } from "@hmx/contracts/FeeCalculator.sol";
+import { BotHandler } from "@hmx/handlers/BotHandler.sol";
+import { CrossMarginHandler } from "@hmx/handlers/CrossMarginHandler.sol";
+import { LimitTradeHandler } from "@hmx/handlers/LimitTradeHandler.sol";
+import { LiquidityHandler } from "@hmx/handlers/LiquidityHandler.sol";
+import { MarketTradeHandler } from "@hmx/handlers/MarketTradeHandler.sol";
 
 contract DeployStorages is ConfigJsonRepo {
   function run() public {
     uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
     vm.startBroadcast(deployerPrivateKey);
 
-    address oracleMiddlewareAddress = getJsonAddress(".oracle.middleware");
-    address vaultStorageAddress = getJsonAddress(".storages.vault");
-    address perpStorageAddress = getJsonAddress(".storages.perp");
-    address configStorageAddress = getJsonAddress(".storages.config");
+    address pythAddress = getJsonAddress(".oracle.pyth");
+    address tradeServiceAddress = getJsonAddress(".services.trade");
+    address liquiditionServiceAddress = getJsonAddress(".services.liquidition");
+    address liquidityServiceAddress = getJsonAddress(".services.liquidity");
+    address crossMarginServiceAddress = getJsonAddress(".services.crossMargin");
+    address weth = getJsonAddress(".tokens.weth");
+    // @todo - TBD
+    uint256 minExecutionFee = 0;
 
-    address calculatorAddress = address(
-      new Calculator(oracleMiddlewareAddress, vaultStorageAddress, perpStorageAddress, configStorageAddress)
+    address botHandlerAddress = address(new BotHandler(tradeServiceAddress, liquiditionServiceAddress, pythAddress));
+    address crossMarginHandlerAddress = address(new CrossMarginHandler(crossMarginServiceAddress, pythAddress));
+    address liquidityHandlerAddress = address(
+      new LiquidityHandler(liquidityServiceAddress, pythAddress, minExecutionFee)
     );
-    address feeCalculatorAddress = address(new FeeCalculator(vaultStorageAddress, configStorageAddress));
+
+    address marketTradeHandlerAddress = address(new MarketTradeHandler(tradeServiceAddress, pythAddress));
+    address limitTradeHandlerAddress = address(
+      new LimitTradeHandler(weth, tradeServiceAddress, pythAddress, minExecutionFee)
+    );
 
     vm.stopBroadcast();
 
-    updateJson(".calculator", calculatorAddress);
-    updateJson(".feeCalculator", feeCalculatorAddress);
+    updateJson(".handlers.bot", botHandlerAddress);
+    updateJson(".handlers.crossMargin", crossMarginHandlerAddress);
+    updateJson(".handlers.limitTrade", limitTradeHandlerAddress);
+    updateJson(".handlers.liquidity", liquidityHandlerAddress);
+    updateJson(".handlers.marketTrade", marketTradeHandlerAddress);
   }
 }

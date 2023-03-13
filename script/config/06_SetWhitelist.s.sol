778439235543069b93dedb5fe1472d474ae96492
Jonathan Xina <john.xina.p88@gmail.com>
2023-03-13 11:29:08 +0700

    feat: add whitelist for perp storage

--- script/config/06_SetWhitelist.s.sol
+++ script/config/06_SetWhitelist.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.18;
 import { ConfigJsonRepo } from "@hmx-script/utils/ConfigJsonRepo.s.sol";
 import { IConfigStorage } from "@hmx/storages/interfaces/IConfigStorage.sol";
 import { IVaultStorage } from "@hmx/storages/interfaces/IVaultStorage.sol";
+import { IPerpStorage } from "@hmx/storages/interfaces/IPerpStorage.sol";
 import { ILimitTradeHandler } from "@hmx/handlers/interfaces/ILimitTradeHandler.sol";
 import { ILiquidityHandler } from "@hmx/handlers/interfaces/ILiquidityHandler.sol";
 
@@ -14,42 +15,43 @@ contract SetWhitelist is ConfigJsonRepo {
 
     address ORDER_EXECUTOR = 0x6629eC35c8Aa279BA45Dbfb575c728d3812aE31a;
 
-    address crossMarginServiceAddress = getJsonAddress(".services.crossMargin");
-    address liquidityServiceAddress = getJsonAddress(".services.liquidity");
-    address liquidationServiceAddress = getJsonAddress(".services.liquidation");
-    address tradeServiceAddress = getJsonAddress(".services.trade");
-
-    address crossMarginHandlerAddress = getJsonAddress(".handlers.crossMargin");
-    address liquidityHandlerAddress = getJsonAddress(".handlers.liquidity");
-    address botHandlerAddress = getJsonAddress(".handlers.bot");
-    address limitTradeHandlerAddress = getJsonAddress(".handlers.limitTrade");
-    address marketTradeHandlerAddress = getJsonAddress(".handlers.marketTrade");
-
     IConfigStorage configStorage = IConfigStorage(getJsonAddress(".storages.config"));
     IVaultStorage vaultStorage = IVaultStorage(getJsonAddress(".storages.vault"));
-    ILimitTradeHandler limitTradeHandler = ILimitTradeHandler(limitTradeHandlerAddress);
-    ILiquidityHandler liquidityHandler = ILiquidityHandler(liquidityHandlerAddress);
+    IPerpStorage perpStorage = IPerpStorage(getJsonAddress(".storages.perp"));
+    ILimitTradeHandler limitTradeHandler = ILimitTradeHandler(getJsonAddress(".handlers.limitTrade"));
+    ILiquidityHandler liquidityHandler = ILiquidityHandler(getJsonAddress(".handlers.liquidity"));
 
     // Set order executors
     limitTradeHandler.setOrderExecutor(ORDER_EXECUTOR, true);
     liquidityHandler.setOrderExecutor(ORDER_EXECUTOR, true);
 
     // Set Cross margin executors
-    configStorage.setServiceExecutor(crossMarginServiceAddress, crossMarginHandlerAddress, true);
+    configStorage.setServiceExecutor(
+      getJsonAddress(".services.crossMargin"),
+      getJsonAddress(".handlers.crossMargin"),
+      true
+    );
 
     // Set Liquidity service executors
-    configStorage.setServiceExecutor(liquidityServiceAddress, liquidityHandlerAddress, true);
+    configStorage.setServiceExecutor(
+      getJsonAddress(".services.crossMargin"),
+      getJsonAddress(".handlers.liquidity"),
+      true
+    );
 
     // Set Liquidation service executors
-    configStorage.setServiceExecutor(liquidationServiceAddress, botHandlerAddress, true);
+    configStorage.setServiceExecutor(getJsonAddress(".services.crossMargin"), getJsonAddress(".handlers.bot"), true);
 
     // Set Trade service executors
-    configStorage.setServiceExecutor(tradeServiceAddress, limitTradeHandlerAddress, true);
-    configStorage.setServiceExecutor(tradeServiceAddress, marketTradeHandlerAddress, true);
-    configStorage.setServiceExecutor(tradeServiceAddress, botHandlerAddress, true);
+    configStorage.setServiceExecutor(getJsonAddress(".services.trade"), getJsonAddress(".handlers.limitTrade"), true);
+    configStorage.setServiceExecutor(getJsonAddress(".services.trade"), getJsonAddress(".handlers.marketTrade"), true);
+    configStorage.setServiceExecutor(getJsonAddress(".services.trade"), getJsonAddress(".handlers.bot"), true);
 
-    vaultStorage.setServiceExecutors(liquidityServiceAddress, true);
-    vaultStorage.setServiceExecutors(crossMarginServiceAddress, true);
+    vaultStorage.setServiceExecutors(getJsonAddress(".services.crossMargin"), true);
+    vaultStorage.setServiceExecutors(getJsonAddress(".services.crossMargin"), true);
+
+    perpStorage.setServiceExecutors(getJsonAddress(".services.trade"), true);
+    perpStorage.setServiceExecutors(getJsonAddress(".services.crossMargin"), true);
 
     vm.stopBroadcast();
   }

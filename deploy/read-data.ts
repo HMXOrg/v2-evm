2efe19959472944ac3d4487bc8a34d97aa54c583
Jonathan Xina <john.xina.p88@gmail.com>
2023-03-16 17:17:59 +0700

    feat: read data with multicall

--- deploy/read-data.ts
+++ deploy/read-data.ts
@@ -1,8 +1,16 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { ethers } from "hardhat";
-import { Calculator__factory, OracleMiddleware__factory, VaultStorage__factory, ERC20__factory, PLPv2__factory, PerpStorage__factory } from "../typechain";
+import { ethers, network } from "hardhat";
+import {
+  Calculator__factory,
+  OracleMiddleware__factory,
+  VaultStorage__factory,
+  ERC20__factory,
+  PLPv2__factory,
+  PerpStorage__factory,
+} from "../typechain";
 import { getConfig } from "./utils/config";
+import { MultiCall, JsonFragment } from "@indexed-finance/multicall";
 
 const BigNumber = ethers.BigNumber;
 const config = getConfig();
@@ -16,102 +24,365 @@ const daiAssetId = "0x0000000000000000000000000000000000000000000000000000000000
 const appleAssetId = "0x0000000000000000000000000000000000000000000000000000000000000006";
 const jpyAssetId = "0x0000000000000000000000000000000000000000000000000000000000000007";
 
-
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const deployer = (await ethers.getSigners())[0];
-  const address = BigNumber.from(deployer.address).xor(subAccountId).toHexString()
-
-  const usdc = ERC20__factory.connect(config.tokens.usdc, deployer)
-  const usdcBalance = await usdc.balanceOf(deployer.address)
-  console.log('usdc balance', ethers.utils.formatUnits(usdcBalance, 6));
-
-  const usdt = ERC20__factory.connect(config.tokens.usdt, deployer)
-  const usdtBalance = await usdt.balanceOf(deployer.address)
-  console.log('usdt balance', ethers.utils.formatUnits(usdtBalance, 6));
-
-  const wbtc = ERC20__factory.connect(config.tokens.wbtc, deployer)
-  const wbtcBalance = await wbtc.balanceOf(deployer.address)
-  console.log('wbtc balance', ethers.utils.formatUnits(wbtcBalance, 8));
-
-  const dai = ERC20__factory.connect(config.tokens.dai, deployer)
-  const daiBalance = await dai.balanceOf(deployer.address)
-  console.log('dai balance', ethers.utils.formatUnits(daiBalance, 18));
-
+  const address = BigNumber.from(deployer.address).xor(subAccountId).toHexString();
   const provider = ethers.provider;
-  console.log('eth balance', ethers.utils.formatUnits(await provider.getBalance(deployer.address), 18));
+  const multi = new MultiCall(provider);
 
+  const balances = await multi.getBalances(
+    [config.tokens.usdc, config.tokens.usdt, config.tokens.dai, config.tokens.wbtc, config.tokens.plp],
+    deployer.address
+  );
+  console.log("=== Wallet Balances ===");
+  console.table([
+    {
+      token: "plp",
+      balance: ethers.utils.formatUnits(balances[1][config.tokens.plp].toString(), 18),
+    },
+    {
+      token: "usdc",
+      balance: ethers.utils.formatUnits(balances[1][config.tokens.usdc].toString(), 6),
+    },
+    {
+      token: "usdt",
+      balance: ethers.utils.formatUnits(balances[1][config.tokens.usdt].toString(), 6),
+    },
+    {
+      token: "dai",
+      balance: ethers.utils.formatUnits(balances[1][config.tokens.dai].toString(), 18),
+    },
+    {
+      token: "wbtc",
+      balance: ethers.utils.formatUnits(balances[1][config.tokens.wbtc].toString(), 8),
+    },
+    {
+      token: "eth",
+      balance: ethers.utils.formatUnits(await provider.getBalance(deployer.address), 18),
+    },
+  ]);
 
-  const vaultStorage = VaultStorage__factory.connect(config.storages.vault, deployer);
-  const perpStorage = PerpStorage__factory.connect(config.storages.perp, deployer);
-  const calculator = Calculator__factory.connect(config.calculator, deployer);
-  const oracle = OracleMiddleware__factory.connect(config.oracle.middleware, deployer)
-  const plp = PLPv2__factory.connect(config.tokens.plp, deployer)
-
-  const traderBalancesUsdc = await vaultStorage.traderBalances(address, config.tokens.usdc)
-  const traderBalancesUsdt = await vaultStorage.traderBalances(address, config.tokens.usdt)
-  const traderBalancesWbtc = await vaultStorage.traderBalances(address, config.tokens.wbtc)
-  const traderBalancesEth = await vaultStorage.traderBalances(address, config.tokens.weth)
-  const traderBalancesDai = await vaultStorage.traderBalances(address, config.tokens.dai)
-
-
-
-  const freeCollateral = await calculator.getFreeCollateral(address, 0, "0x0000000000000000000000000000000000000000000000000000000000000000")
-  const equity = await calculator.getEquity(address, 0, "0x0000000000000000000000000000000000000000000000000000000000000000")
-  const usdcPrice = (await oracle.getLatestPrice(usdcAssetId, false))._price;
-  const usdtPrice = (await oracle.getLatestPrice(usdtAssetId, false))._price;
-  const wbtcPrice = (await oracle.getLatestPrice(wbtcAssetId, false))._price;
-  const daiPrice = (await oracle.getLatestPrice(daiAssetId, false))._price;
-  const ethPrice = (await oracle.getLatestPrice(ethAssetId, false))._price;
-  const applPrice = (await oracle.getLatestPrice(appleAssetId, false))._price;
-  const jpyPrice = (await oracle.getLatestPrice(jpyAssetId, false))._price;
-
-  const plpBalance = await plp.balanceOf(deployer.address)
-
-  console.log("========Price========");
-  console.log(ethers.utils.formatUnits(usdcPrice, 30));//USDCPrice
-  console.log(ethers.utils.formatUnits(usdtPrice, 30));//USDTPrice
-  console.log(ethers.utils.formatUnits(daiPrice, 30));//DAIPrice
-  console.log(ethers.utils.formatUnits(ethPrice, 30));//ETHPrice
-  console.log(ethers.utils.formatUnits(wbtcPrice, 30));//WBTCPrice
-  console.log(ethers.utils.formatUnits(applPrice, 30));//APPLPrice
-  console.log(ethers.utils.formatUnits(jpyPrice, 30));//JPYPrice
-
-  console.log("=====================");
-  console.log("traderBalancesUSDC", ethers.utils.formatUnits(traderBalancesUsdc, 6));
-  console.log("traderBalancesUSDT", ethers.utils.formatUnits(traderBalancesUsdt, 6));
-  console.log("traderBalancesETH", ethers.utils.formatUnits(traderBalancesEth, 18));
-  console.log("traderBalancesWBTC", ethers.utils.formatUnits(traderBalancesWbtc, 8));
-  console.log("traderBalancesDAI", ethers.utils.formatUnits(traderBalancesDai, 18));
-  console.log("=====================");
-  console.log("equity", ethers.utils.formatUnits(equity, 30));
-  console.log("freeCollateral", ethers.utils.formatUnits(freeCollateral, 30));
-  console.log("=========PLP============");
-  console.log("plpBalance", ethers.utils.formatUnits(plpBalance, 18));
-  const plpTotalSupply = await plp.totalSupply();
-  const plpAum = await calculator.getAUME30(true, 0, ethAssetId);
-  console.log("plpTotalSupply", ethers.utils.formatUnits(plpTotalSupply, 18));
-  console.log("plpAum", ethers.utils.formatUnits(plpAum, 30))
-  console.log("plpPrice", ethers.utils.formatUnits(plpAum.mul(ethers.utils.parseEther("1")).div(plpTotalSupply), 30));
-  console.log("plpLiquidity USDC", ethers.utils.formatUnits(await vaultStorage.plpLiquidity(config.tokens.usdc), 6));
-  console.log("plpLiquidity USDT", ethers.utils.formatUnits(await vaultStorage.plpLiquidity(config.tokens.usdt), 6));
-  console.log("plpLiquidity WBTC", ethers.utils.formatUnits(await vaultStorage.plpLiquidity(config.tokens.wbtc), 8));
-  console.log("plpLiquidity ETH", ethers.utils.formatUnits(await vaultStorage.plpLiquidity(config.tokens.weth), 18));
-  // console.log("Crypto Reserved Value", await (await perpStorage.globalAssetClass(0)).reserveValueE30)
-  console.log("Equity Reserved Value", await (await perpStorage.globalAssetClass(1)).reserveValueE30)
-  // console.log("Forex Reserved Value", await (await perpStorage.globalAssetClass(2)).reserveValueE30)
-  console.log("=========platform fee============");
-  console.log("fees USDC", ethers.utils.formatUnits(await vaultStorage.fees(config.tokens.usdc), 6));
-  console.log("fees USDT", ethers.utils.formatUnits(await vaultStorage.fees(config.tokens.usdt), 6));
-  console.log("fees WBTC", ethers.utils.formatUnits(await vaultStorage.fees(config.tokens.wbtc), 8));
-  console.log("fees ETH", ethers.utils.formatUnits(await vaultStorage.fees(config.tokens.weth), 18));
-  console.log("=========dev fee============");
-  console.log("devfees USDC", ethers.utils.formatUnits(await vaultStorage.devFees(config.tokens.usdc), 6));
-  console.log("devfees USDT", ethers.utils.formatUnits(await vaultStorage.devFees(config.tokens.usdt), 6));
-  console.log("devfees WBTC", ethers.utils.formatUnits(await vaultStorage.devFees(config.tokens.wbtc), 8));
-  console.log("devfees ETH", ethers.utils.formatUnits(await vaultStorage.devFees(config.tokens.weth), 18));
-
-  console.log((await perpStorage.globalMarkets(0)).longOpenInterest)
-
+  const inputs = [
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "traderBalances",
+      args: [deployer.address, config.tokens.usdc],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "traderBalances",
+      args: [deployer.address, config.tokens.usdt],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "traderBalances",
+      args: [deployer.address, config.tokens.dai],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "traderBalances",
+      args: [deployer.address, config.tokens.weth],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "traderBalances",
+      args: [deployer.address, config.tokens.wbtc],
+    },
+    // Equity
+    {
+      interface: Calculator__factory.abi,
+      target: config.calculator,
+      function: "getEquity",
+      args: [deployer.address, 0, ethAssetId],
+    },
+    // Free Collateral
+    {
+      interface: Calculator__factory.abi,
+      target: config.calculator,
+      function: "getFreeCollateral",
+      args: [deployer.address, 0, ethAssetId],
+    },
+    // Prices
+    {
+      interface: OracleMiddleware__factory.abi,
+      target: config.oracle.middleware,
+      function: "getLatestPrice",
+      args: [usdcAssetId, false],
+    },
+    {
+      interface: OracleMiddleware__factory.abi,
+      target: config.oracle.middleware,
+      function: "getLatestPrice",
+      args: [usdtAssetId, false],
+    },
+    {
+      interface: OracleMiddleware__factory.abi,
+      target: config.oracle.middleware,
+      function: "getLatestPrice",
+      args: [daiAssetId, false],
+    },
+    {
+      interface: OracleMiddleware__factory.abi,
+      target: config.oracle.middleware,
+      function: "getLatestPrice",
+      args: [ethAssetId, false],
+    },
+    {
+      interface: OracleMiddleware__factory.abi,
+      target: config.oracle.middleware,
+      function: "getLatestPrice",
+      args: [wbtcAssetId, false],
+    },
+    {
+      interface: OracleMiddleware__factory.abi,
+      target: config.oracle.middleware,
+      function: "getLatestPrice",
+      args: [appleAssetId, false],
+    },
+    {
+      interface: OracleMiddleware__factory.abi,
+      target: config.oracle.middleware,
+      function: "getLatestPrice",
+      args: [jpyAssetId, false],
+    },
+    // PLP
+    {
+      interface: PLPv2__factory.abi,
+      target: config.tokens.plp,
+      function: "totalSupply",
+      args: [],
+    },
+    {
+      interface: Calculator__factory.abi,
+      target: config.calculator,
+      function: "getAUME30",
+      args: [true, 0, ethAssetId],
+    },
+    // PLP Liquidity
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "plpLiquidity",
+      args: [config.tokens.usdc],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "plpLiquidity",
+      args: [config.tokens.usdt],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "plpLiquidity",
+      args: [config.tokens.dai],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "plpLiquidity",
+      args: [config.tokens.weth],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "plpLiquidity",
+      args: [config.tokens.wbtc],
+    },
+    // Asset Class
+    {
+      interface: PerpStorage__factory.abi,
+      target: config.storages.perp,
+      function: "globalAssetClass",
+      args: [0],
+    },
+    {
+      interface: PerpStorage__factory.abi,
+      target: config.storages.perp,
+      function: "globalAssetClass",
+      args: [1],
+    },
+    {
+      interface: PerpStorage__factory.abi,
+      target: config.storages.perp,
+      function: "globalAssetClass",
+      args: [2],
+    },
+    // Fees
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "fees",
+      args: [config.tokens.usdc],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "fees",
+      args: [config.tokens.usdt],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "fees",
+      args: [config.tokens.dai],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "fees",
+      args: [config.tokens.weth],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "fees",
+      args: [config.tokens.wbtc],
+    },
+    // Dev Fees
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "devFees",
+      args: [config.tokens.usdc],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "devFees",
+      args: [config.tokens.usdt],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "devFees",
+      args: [config.tokens.dai],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "devFees",
+      args: [config.tokens.weth],
+    },
+    {
+      interface: VaultStorage__factory.abi,
+      target: config.storages.vault,
+      function: "devFees",
+      args: [config.tokens.wbtc],
+    },
+  ];
+  const [
+    blockNumber,
+    [
+      traderBalancesUsdc,
+      traderBalancesUsdt,
+      traderBalancesDai,
+      traderBalancesWeth,
+      traderBalancesWbtc,
+      equity,
+      freeCollateral,
+      usdcPrice,
+      usdtPrice,
+      daiPrice,
+      wethPrice,
+      wbtcPrice,
+      applePrice,
+      jpyPrice,
+      plpTotalSupply,
+      plpAum,
+      plpLiquidityUsdc,
+      plpLiquidityUsdt,
+      plpLiquidityDai,
+      plpLiquidityWeth,
+      plpLiquidityWbtc,
+      assetClassCrypto,
+      assetClassEquity,
+      assetClassForex,
+      feeUsdc,
+      feeUsdt,
+      feeDai,
+      feeWeth,
+      feeWbtc,
+      devFeeUsdc,
+      devFeeUsdt,
+      devFeeDai,
+      devFeeWeth,
+      devFeeWbtc,
+    ],
+  ] = await multi.multiCall(inputs);
+  console.log("=== Prices ===");
+  console.log(ethers.utils.formatUnits(usdcPrice._price, 30));
+  console.log(ethers.utils.formatUnits(usdtPrice._price, 30));
+  console.log(ethers.utils.formatUnits(daiPrice._price, 30));
+  console.log(ethers.utils.formatUnits(wethPrice._price, 30));
+  console.log(ethers.utils.formatUnits(wbtcPrice._price, 30));
+  console.log(ethers.utils.formatUnits(applePrice._price, 30));
+  console.log(ethers.utils.formatUnits(jpyPrice._price, 30));
+  console.log("=== Cross Margin Account ===");
+  console.table({
+    equity: ethers.utils.formatUnits(equity, 30),
+    freeCollateral: ethers.utils.formatUnits(freeCollateral, 30),
+  });
+  console.log("=== Trader Balances ===");
+  console.table({
+    usdc: ethers.utils.formatUnits(traderBalancesUsdc, 6),
+    usdt: ethers.utils.formatUnits(traderBalancesUsdt, 6),
+    dai: ethers.utils.formatUnits(traderBalancesDai, 18),
+    weth: ethers.utils.formatUnits(traderBalancesWeth, 18),
+    wbtc: ethers.utils.formatUnits(traderBalancesWbtc, 8),
+  });
+  console.log("=== PLP ===");
+  console.table({
+    plpTotalSupply: ethers.utils.formatUnits(plpTotalSupply, 18),
+    plpAum: ethers.utils.formatUnits(plpAum, 30),
+    plpPrice: plpAum.gt(0)
+      ? ethers.utils.formatUnits(plpAum.mul(ethers.utils.parseEther("1")).div(plpTotalSupply), 30)
+      : 0,
+    usdc: ethers.utils.formatUnits(plpLiquidityUsdc, 6),
+    usdt: ethers.utils.formatUnits(plpLiquidityUsdt, 6),
+    dai: ethers.utils.formatUnits(plpLiquidityDai, 6),
+    weth: ethers.utils.formatUnits(plpLiquidityWeth, 6),
+    wbtc: ethers.utils.formatUnits(plpLiquidityWbtc, 6),
+  });
+  console.log("=== Asset Class ====");
+  console.table({
+    crypto: {
+      reservedValue: ethers.utils.formatUnits(assetClassCrypto.reserveValueE30, 30),
+      sumBorrowingRate: assetClassCrypto.sumBorrowingRate,
+      lastBorrowingTime: assetClassCrypto.lastBorrowingTime,
+    },
+    equity: {
+      reservedValue: ethers.utils.formatUnits(assetClassEquity.reserveValueE30, 30),
+      sumBorrowingRate: assetClassEquity.sumBorrowingRate,
+      lastBorrowingTime: assetClassEquity.lastBorrowingTime,
+    },
+    forex: {
+      reservedValue: ethers.utils.formatUnits(assetClassForex.reserveValueE30, 30),
+      sumBorrowingRate: assetClassForex.sumBorrowingRate,
+      lastBorrowingTime: assetClassForex.lastBorrowingTime,
+    },
+  });
+  console.log("=== Platform Fees ===");
+  console.table({
+    usdc: ethers.utils.formatUnits(feeUsdc, 6),
+    usdt: ethers.utils.formatUnits(feeUsdt, 6),
+    dai: ethers.utils.formatUnits(feeDai, 6),
+    weth: ethers.utils.formatUnits(feeWeth, 6),
+    wbtc: ethers.utils.formatUnits(feeWbtc, 6),
+  });
+  console.log("=== Dev Fees ===");
+  console.table({
+    usdc: ethers.utils.formatUnits(devFeeUsdc, 6),
+    usdt: ethers.utils.formatUnits(devFeeUsdt, 6),
+    dai: ethers.utils.formatUnits(devFeeDai, 6),
+    weth: ethers.utils.formatUnits(devFeeWeth, 6),
+    wbtc: ethers.utils.formatUnits(devFeeWbtc, 6),
+  });
 };
 
 export default func;

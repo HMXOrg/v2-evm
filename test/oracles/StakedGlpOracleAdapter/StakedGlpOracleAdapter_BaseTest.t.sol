d77af94e55ad71f70404fce377929d08da9b4a4a
ct <chentang88158@gmail.com>
2023-04-20 15:34:05 +0700

    Revert "feat: remove test"
    This reverts commit 15584ea7868826d41c6363a7ab98bf1eb5d40a5b.

---
+++ test/oracles/StakedGlpOracleAdapter/StakedGlpOracleAdapter_BaseTest.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import { BaseTest } from "@hmx-test/base/BaseTest.sol";
+import { StakedGlpOracleAdapter } from "@hmx/oracles/StakedGlpOracleAdapter.sol";
+
+contract StakedGlpOracleAdapter_BaseTest is BaseTest {
+  function setUp() public virtual {}
+}

3f8dd47208902eeddf4eda2628b562188e0a9854
ct <chentang88158@gmail.com>
2023-04-20 15:34:17 +0700

    chore: fix incorrect import

--- test/fork/StakedGlpOracleAdapter_GetLatestPrice.t.fork.sol
+++ test/fork/StakedGlpOracleAdapter_GetLatestPrice.t.fork.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.18;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IGmxGlpManager } from "@hmx/interfaces/gmx/IGmxGlpManager.sol";
-import { StakedGlpOracleAdapter } from "@hmx/oracles/stakedGlpOracleAdapter.sol";
+import { StakedGlpOracleAdapter } from "@hmx/oracles/StakedGlpOracleAdapter.sol";
 
 import { TestBase } from "forge-std/Base.sol";
 import { StdCheatsSafe } from "forge-std/StdCheats.sol";

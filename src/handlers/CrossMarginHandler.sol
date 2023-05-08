1f4b930a3f697d4a5f3dec342abcbb81536fe412
Kim Hera <kim.hera.20.20.20@gmail.com>
2023-05-08 17:12:57 +0700

    chore: remove console.log

--- src/handlers/CrossMarginHandler.sol
+++ src/handlers/CrossMarginHandler.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
-import { console2 } from "forge-std/console2.sol";
 
 // base
 import { ERC20Upgradeable } from "@openzeppelin-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";

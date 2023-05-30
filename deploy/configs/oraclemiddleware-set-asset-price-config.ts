394ce8e5c17dfb58764c83d1255c0f098d4c1f48
Jonathan Xina <john.xina.p88@gmail.com>
2023-05-30 11:39:31 +0700

    chore: 15 seconds stale

--- deploy/configs/oraclemiddleware-set-asset-price-config.ts
+++ deploy/configs/oraclemiddleware-set-asset-price-config.ts
@@ -18,56 +18,52 @@ const assetConfigs = [
   {
     assetId: ethers.utils.formatBytes32String("ETH"),
     confidenceThreshold: 0,
-    trustPriceAge: 60 * 60 * 24 * 365,
+    trustPriceAge: 15,
     adapter: config.oracles.pythAdapter,
   },
   {
     assetId: ethers.utils.formatBytes32String("BTC"),
     confidenceThreshold: 0,
-    trustPriceAge: 60 * 60 * 24 * 365,
+    trustPriceAge: 15,
     adapter: config.oracles.pythAdapter,
   },
   {
     assetId: ethers.utils.formatBytes32String("USDC"),
     confidenceThreshold: 0,
-    trustPriceAge: 60 * 60 * 24 * 365,
+    trustPriceAge: 15,
     adapter: config.oracles.pythAdapter,
   },
   {
     assetId: ethers.utils.formatBytes32String("USDT"),
     confidenceThreshold: 0,
-    trustPriceAge: 60 * 60 * 24 * 365,
+    trustPriceAge: 15,
     adapter: config.oracles.pythAdapter,
   },
   {
     assetId: ethers.utils.formatBytes32String("DAI"),
     confidenceThreshold: 0,
-    trustPriceAge: 60 * 60 * 24 * 365,
+    trustPriceAge: 15,
     adapter: config.oracles.pythAdapter,
   },
   {
     assetId: ethers.utils.formatBytes32String("AAPL"),
     confidenceThreshold: 0,
-    trustPriceAge: 60 * 60 * 24 * 365,
+    trustPriceAge: 15,
     adapter: config.oracles.pythAdapter,
   },
   {
     assetId: ethers.utils.formatBytes32String("JPY"),
     confidenceThreshold: 0,
-    trustPriceAge: 60 * 60 * 24 * 365,
+    trustPriceAge: 15,
     adapter: config.oracles.pythAdapter,
   },
   {
     assetId: ethers.utils.formatBytes32String("GLP"),
     confidenceThreshold: 0,
-    trustPriceAge: 60 * 60 * 24 * 365,
+    trustPriceAge: 15,
     adapter: config.oracles.pythAdapter,
   },
 ];
-const assetId = ethers.utils.formatBytes32String("GLP");
-const confidenceThreshold = 0; // UNUSED
-const trustPriceAge = 15; // 15 seconds
-const adapter = config.oracles.pythAdapter;
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const deployer = (await ethers.getSigners())[0];

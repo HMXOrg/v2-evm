db019261e3a617ea59a517eccb56518a9f5e88e0
Jonathan Xina <john.xina.p88@gmail.com>
2023-06-16 12:28:37 +0700

    deploy: upgrade all stuff from audit findings

---
+++ script/ts/upgrade/CrossMarginService/index.ts
@@ -0,0 +1,37 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers, tenderly, upgrades, network } from "hardhat";
+import { getConfig, writeConfigFile } from "../../utils/config";
+import { getImplementationAddress } from "@openzeppelin/upgrades-core";
+
+const BigNumber = ethers.BigNumber;
+const config = getConfig();
+
+async function main() {
+  const deployer = (await ethers.getSigners())[0];
+
+  const Contract = await ethers.getContractFactory("CrossMarginService", deployer);
+  const TARGET_ADDRESS = config.services.crossMargin;
+
+  console.log(`> Preparing to upgrade CrossMarginService`);
+  const newImplementation = await upgrades.prepareUpgrade(TARGET_ADDRESS, Contract);
+  console.log(`> Done`);
+
+  console.log(`> New CrossMarginService Implementation address: ${newImplementation}`);
+  const upgradeTx = await upgrades.upgradeProxy(TARGET_ADDRESS, Contract);
+  console.log(`> ⛓ Tx is submitted: ${upgradeTx.deployTransaction.hash}`);
+  console.log(`> Waiting for tx to be mined...`);
+  await upgradeTx.deployTransaction.wait(3);
+  console.log(`> Tx is mined!`);
+
+  console.log(`> Verify contract on Tenderly`);
+  await tenderly.verify({
+    address: newImplementation.toString(),
+    name: "CrossMarginService",
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});

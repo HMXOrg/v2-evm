b59e60d6160fa5c0818cbfdd161ab7e65769aa3d
Jonathan Xina <john.xina.p88@gmail.com>
2023-06-19 17:22:08 +0700

    deploy: deploy limit trade handelr with new multicall

--- script/ts/deploy/LimitTradeHandler/index.ts
+++ script/ts/deploy/LimitTradeHandler/index.ts
@@ -5,20 +5,20 @@ import { getImplementationAddress } from "@openzeppelin/upgrades-core";
 const BigNumber = ethers.BigNumber;
 const config = getConfig();
 
-const minExecutionFee = 30; // 30 wei
+const minExecutionFee = ethers.utils.parseEther("0.0003"); // 0.0003 ether
 const minExecutionTimestamp = 60 * 60 * 5; // 5 minutes
 
 async function main() {
   const deployer = (await ethers.getSigners())[0];
 
   const Contract = await ethers.getContractFactory("LimitTradeHandler", deployer);
-  const contract = await upgrades.deployProxy(Contract, [
-    config.tokens.weth,
-    config.services.trade,
-    config.oracles.ecoPyth,
-    minExecutionFee,
-    minExecutionTimestamp,
-  ]);
+  const contract = await upgrades.deployProxy(
+    Contract,
+    [config.tokens.weth, config.services.trade, config.oracles.ecoPyth, minExecutionFee, minExecutionTimestamp],
+    {
+      unsafeAllow: ["delegatecall"],
+    }
+  );
   await contract.deployed();
   console.log(`Deploying LimitTradeHandler Contract`);
   console.log(`Deployed at: ${contract.address}`);

a0bbf2412bbf4df5022374fdc5c56acf81b354f1
Jonathan Xina <john.xina.p88@gmail.com>
2023-07-11 13:56:36 +0700

    feat: mint token

---
+++ script/ts/configs/MockToken/mint-token.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+import { MockErc20__factory } from "../../../../typechain";
+import { getConfig } from "../../utils/config";
+
+const config = getConfig();
+
+const receiver = "0x6629eC35c8Aa279BA45Dbfb575c728d3812aE31a";
+const mintAmount = ethers.utils.parseUnits("1000000", 6);
+
+async function main() {
+  const deployer = (await ethers.getSigners())[0];
+  const token = MockErc20__factory.connect(config.tokens.usdc, deployer);
+
+  console.log("> Mint Token...");
+  await (await token.mint(receiver, mintAmount)).wait();
+  console.log("> Mint Token success!");
+}
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});

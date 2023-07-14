0561d022c01836cd7d15f82f3f1cdec78d771b65
Asaki-Katsu <boom@100x.fi>
2023-07-14 20:43:47 +0700

    update: setWhiteList on deploy, reducing steps

--- script/ts/deploys/RebalanceHLPHandler/index.ts
+++ script/ts/deploys/RebalanceHLPHandler/index.ts
@@ -1,5 +1,7 @@
 import { ethers, tenderly, upgrades, network } from "hardhat";
 import { getConfig, writeConfigFile } from "../../utils/config";
+import { RebalanceHLPHandler__factory } from "../../../../typechain";
+import signers from "../../entities/signers";
 import { getImplementationAddress } from "@openzeppelin/upgrades-core";
 
 const BigNumber = ethers.BigNumber;
@@ -23,6 +25,8 @@ async function main() {
   console.log(`Deploying RebalanceHLPHandler Contract`);
   console.log(`Deployed at: ${contract.address}`);
 
+  const user = "0x05bDb067630e19e7e4aBF3436AF0e176Be573D32";
+
   config.handlers.rebalanceHLP = contract.address;
   writeConfigFile(config);
 
@@ -30,6 +34,11 @@ async function main() {
     address: await getImplementationAddress(network.provider, contract.address),
     name: "RebalanceHLPHandler",
   });
+
+  const handler = RebalanceHLPHandler__factory.connect(config.handlers.rebalanceHLP, signers.deployer(42161));
+  const tx = await handler.setWhiteListExecutor(user, true, { gasLimit: 10000000 });
+  await tx.wait(1);
+  console.log(`Set whitelist to address: ${user}`);
 }
 
 main().catch((error) => {

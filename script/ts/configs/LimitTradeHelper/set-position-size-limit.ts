7342dc22a545fd64a53f18cff3927b9d3a87919d
Jonathan Xina <john.xina.p88@gmail.com>
2023-07-20 11:03:48 +0700

    feat: naming and batch call

--- script/ts/configs/LimitTradeHelper/set-position-size-limit.ts
+++ script/ts/configs/LimitTradeHelper/set-position-size-limit.ts
@@ -9,13 +9,37 @@ async function main(chainId: number) {
   const config = loadConfig(chainId);
   const deployer = signers.deployer(chainId);
 
-  const assetClass = assetClasses.crypto;
+  const inputs = [
+    {
+      assetClass: assetClasses.crypto,
+      tradeSizeLimit: ethers.utils.parseUnits("300000", 30), // 300k
+      positionSizeLimit: ethers.utils.parseUnits("500000", 30), // 500k
+    },
+    {
+      assetClass: assetClasses.equity,
+      tradeSizeLimit: ethers.utils.parseUnits("300000", 30), // 300k
+      positionSizeLimit: ethers.utils.parseUnits("500000", 30), // 500k
+    },
+    {
+      assetClass: assetClasses.commodities,
+      tradeSizeLimit: ethers.utils.parseUnits("300000", 30), // 300k
+      positionSizeLimit: ethers.utils.parseUnits("500000", 30), // 500k
+    },
+    {
+      assetClass: assetClasses.forex,
+      tradeSizeLimit: ethers.utils.parseUnits("1000000", 30), // 1M
+      positionSizeLimit: ethers.utils.parseUnits("2500000", 30), // 2.5M
+    },
+  ];
+  const assetClass = assetClasses.commodities;
   const positionSizeLimit = ethers.utils.parseUnits("500000", 30);
   const tradeSizeLimit = ethers.utils.parseUnits("300000", 30);
 
   console.log("> LimitTradeHelper: Set Position and Trade Size Limit...");
   const limitTradeHelper = LimitTradeHelper__factory.connect(config.helpers.limitTrade, deployer);
-  await (await limitTradeHelper.setPositionSizeLimit(assetClass, positionSizeLimit, tradeSizeLimit)).wait();
+  const tx = await limitTradeHelper.setPositionSizeLimit(assetClass, positionSizeLimit, tradeSizeLimit);
+  console.log(`Tx: ${tx.hash}`);
+  await tx.wait();
   console.log("> LimitTradeHelper: Set Position and Trade Size Limit success!");
 }
 

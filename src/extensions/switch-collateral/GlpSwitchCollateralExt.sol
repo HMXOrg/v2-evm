f695c8fb6e9facb74d329bc95dcbd42d6c494218
shutsuwei <shu.tsu.wei88@gmail.com>
2023-07-20 17:02:06 +0700

    chroe: fix CI failed

--- src/extensions/switch-collateral/GlpSwitchCollateralExt.sol
+++ src/extensions/switch-collateral/GlpSwitchCollateralExt.sol
@@ -7,7 +7,7 @@ pragma solidity 0.8.18;
 // libs
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERc20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 // interfaces
 import { IConfigStorage } from "@hmx/storages/interfaces/IConfigStorage.sol";

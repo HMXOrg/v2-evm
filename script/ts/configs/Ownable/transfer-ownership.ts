c16f70518bf96526831bf78cefaaa7c269b91887
Jonathan Xina <john.xina.p88@gmail.com>
2023-08-07 12:26:22 +0700

    feat: script to transfer ownership

---
+++ script/ts/configs/Ownable/transfer-ownership.ts
@@ -0,0 +1,71 @@
+import { OwnableUpgradeable__factory } from "../../../../typechain";
+import { loadConfig } from "../../utils/config";
+import { Command } from "commander";
+import signers from "../../entities/signers";
+import * as readlineSync from "readline-sync";
+
+async function main(chainId: number) {
+  const config = loadConfig(chainId);
+  const deployer = signers.deployer(chainId);
+
+  const newOwner = config.safe;
+  const confirm = readlineSync.question(`Confirm new owner is ${newOwner}? (y/n): `);
+  switch (confirm) {
+    case "y":
+      break;
+    case "n":
+      console.log("Cancelled!");
+      return;
+    default:
+      console.log("Invalid input!");
+      return;
+  }
+
+  const contracts = [
+    config.timelock,
+    config.storages.config,
+    config.storages.perp,
+    config.storages.vault,
+    config.handlers.bot,
+    config.handlers.crossMargin,
+    config.handlers.limitTrade,
+    config.handlers.liquidity,
+    config.oracles.ecoPyth,
+    config.oracles.ecoPyth2,
+    config.oracles.ecoPythCalldataBuilder,
+    config.oracles.middleware,
+    config.oracles.pythAdapter,
+    config.oracles.sglpStakedAdapter,
+    config.oracles.unsafeEcoPythCalldataBuilder,
+    config.tokens.hlp,
+    config.strategies.stakedGlpStrategy,
+    config.strategies.convertedGlpStrategy,
+    config.calculator,
+    config.rewardDistributor,
+  ];
+
+  console.log(`Transfer Ownership to ${newOwner}...`);
+  for (let i = 0; i < contracts.length; i++) {
+    const contract = OwnableUpgradeable__factory.connect(contracts[i], deployer);
+    const tx = await contract.transferOwnership(newOwner);
+    console.log(`Tx: ${tx.hash}`);
+  }
+  console.log("Transfer Ownership Finished");
+}
+
+const prog = new Command();
+
+prog.requiredOption("--chain-id <number>", "chain id", parseInt);
+
+prog.parse(process.argv);
+
+const opts = prog.opts();
+
+main(opts.chainId)
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
---
+++ script/ts/queries/query-owners.ts
@@ -0,0 +1,72 @@
+import { BigNumber, ethers } from "ethers";
+import chains from "../entities/chains";
+import { loadConfig } from "../utils/config";
+import MultiCall from "@indexed-finance/multicall";
+import { OwnableUpgradeable__factory } from "../../../typechain";
+
+const ONE_ETHER = ethers.utils.parseEther("1");
+const formatUnits = ethers.utils.formatUnits;
+
+async function main() {
+  const chain = chains[42161];
+  const config = loadConfig(42161);
+  const provider = new ethers.providers.JsonRpcProvider(chain.rpc);
+  const multicall = new MultiCall(provider);
+
+  const inputs = [
+    { target: config.timelock, function: "owner", args: [] },
+    { target: config.storages.config, function: "owner", args: [] },
+    { target: config.storages.perp, function: "owner", args: [] },
+    { target: config.storages.vault, function: "owner", args: [] },
+    { target: config.handlers.bot, function: "owner", args: [] },
+    { target: config.handlers.crossMargin, function: "owner", args: [] },
+    { target: config.handlers.limitTrade, function: "owner", args: [] },
+    { target: config.handlers.liquidity, function: "owner", args: [] },
+    { target: config.oracles.ecoPyth, function: "owner", args: [] },
+    { target: config.oracles.ecoPyth2, function: "owner", args: [] },
+    { target: config.oracles.ecoPythCalldataBuilder, function: "owner", args: [] },
+    { target: config.oracles.middleware, function: "owner", args: [] },
+    { target: config.oracles.pythAdapter, function: "owner", args: [] },
+    { target: config.oracles.sglpStakedAdapter, function: "owner", args: [] },
+    { target: config.oracles.unsafeEcoPythCalldataBuilder, function: "owner", args: [] },
+    { target: config.tokens.hlp, function: "owner", args: [] },
+    { target: config.strategies.stakedGlpStrategy, function: "owner", args: [] },
+    { target: config.strategies.convertedGlpStrategy, function: "owner", args: [] },
+    { target: config.calculator, function: "owner", args: [] },
+    { target: config.rewardDistributor, function: "owner", args: [] },
+  ];
+
+  const [, owners] = await multicall.multiCall(OwnableUpgradeable__factory.createInterface(), inputs);
+  console.log(owners);
+  console.table({
+    timelock: owners[0],
+    configStorage: owners[1],
+    perpStorage: owners[2],
+    vaultStorage: owners[3],
+    botHandler: owners[4],
+    crossMarginHandler: owners[5],
+    limitTradeHandler: owners[6],
+    liquidityHandler: owners[7],
+    ecoPyth: owners[8],
+    ecoPyth2: owners[9],
+    ecoPythCalldataBuilder: owners[10],
+    oracleMiddleware: owners[11],
+    pythAdapter: owners[12],
+    sglpStakedAdapter: owners[13],
+    unsafeEcoPythCalldataBuilder: owners[14],
+    hlp: owners[15],
+    stakedGlpStrategy: owners[16],
+    convertedGlpStrategy: owners[17],
+    calculator: owners[18],
+    rewardDistributor: owners[19],
+  });
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });

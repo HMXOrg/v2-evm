69e259761756e64097f7f87f7a2e9b5aafcab061
Asaki-Katsu <boom@100x.fi>
2023-08-15 16:56:17 +0700

    refactor: remove unnecessary functions

--- src/extensions/switch-collateral/interfaces/ISwitchCollateralRouter.sol
+++ src/extensions/switch-collateral/interfaces/ISwitchCollateralRouter.sol
@@ -5,8 +5,4 @@ import { IDexter } from "@hmx/extensions/dexters/interfaces/IDexter.sol";
 
 interface ISwitchCollateralRouter {
   function execute(uint256 _amount, address[] calldata _path) external returns (uint256);
-
-  function dexterOf(address _tokenIn, address _tokenOut) external view returns (IDexter _dexter);
-
-  function setDexterOf(address _tokenIn, address _tokenOut, address _switchCollateralExt) external;
 }

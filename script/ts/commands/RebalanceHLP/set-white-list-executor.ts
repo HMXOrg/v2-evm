f9a937f6b4402b27538d87cc0e0d4d5c4c42ba9b
Asaki-Katsu <boom@100x.fi>
2023-08-17 11:48:55 +0700

    chore: config executor to QA acc

--- script/ts/commands/RebalanceHLP/set-white-list-executor.ts
+++ script/ts/commands/RebalanceHLP/set-white-list-executor.ts
@@ -3,7 +3,7 @@ import { RebalanceHLPHandler__factory } from "../../../../typechain";
 import signers from "../../entities/signers";
 
 async function main() {
-  const user = "0x05bDb067630e19e7e4aBF3436AF0e176Be573D32";
+  const user = "0x6629eC35c8Aa279BA45Dbfb575c728d3812aE31a";
   const config = getConfig();
   const handler = RebalanceHLPHandler__factory.connect(config.handlers.rebalanceHLP, signers.deployer(42161));
   const tx = await handler.setWhiteListExecutor(user, true, { gasLimit: 10000000 });

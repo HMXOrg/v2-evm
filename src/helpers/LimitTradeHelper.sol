f0b4a3a059029f3bb56c0d2ea79c3976cf26c689
shutsuwei <shu.tsu.wei88@gmail.com>
2023-08-22 14:52:03 +0700

    chore: improve limit trade helper logic

--- src/helpers/LimitTradeHelper.sol
+++ src/helpers/LimitTradeHelper.sol
@@ -10,20 +10,16 @@ import { ConfigStorage } from "@hmx/storages/ConfigStorage.sol";
 import { PerpStorage } from "@hmx/storages/PerpStorage.sol";
 
 contract LimitTradeHelper is Ownable {
-  error LimitTradeHelper_MarketMaxTradeSize();
   error LimitTradeHelper_MaxTradeSize();
-  error LimitTradeHelper_MarketMaxPositionSize();
   error LimitTradeHelper_MaxPositionSize();
 
-  event LogSetPositionSizeLimit(uint8 _assetClass, uint256 _positionSizeLimit, uint256 _tradeSizeLimit);
-  event LogSetTradeLimitByMarket(uint256 _marketIndex, uint256 _positionSizeLimit, uint256 _tradeSizeLimit);
+  event LogSetLimit(uint256 _marketIndex, uint256 _positionSizeLimitOf, uint256 _tradeSizeLimitOf);
 
   ConfigStorage public configStorage;
   PerpStorage public perpStorage;
-  mapping(uint8 assetClass => uint256 sizeLimit) public positionSizeLimit;
-  mapping(uint8 assetClass => uint256 sizeLimit) public tradeSizeLimit;
-  mapping(uint256 marketIndex => uint256 sizeLimit) public positionSizeLimitByMarket;
-  mapping(uint256 marketIndex => uint256 sizeLimit) public tradeSizeLimitByMarket;
+
+  mapping(uint256 marketIndex => uint256 sizeLimit) public positionSizeLimitOf;
+  mapping(uint256 marketIndex => uint256 sizeLimit) public tradeSizeLimitOf;
 
   constructor(address _configStorage, address _perpStorage) {
     configStorage = ConfigStorage(_configStorage);
@@ -39,48 +35,30 @@ contract LimitTradeHelper is Ownable {
     bool isRevert
   ) external view returns (bool) {
     address _subAccount = HMXLib.getSubAccount(mainAccount, subAccountId);
-    uint8 _assetClass = configStorage.getMarketConfigByIndex(marketIndex).assetClass;
     int256 _positionSizeE30 = perpStorage
       .getPositionById(HMXLib.getPositionId(_subAccount, marketIndex))
       .positionSizeE30;
 
     // Check trade size limit as per market
-    if (
-      tradeSizeLimitByMarket[marketIndex] > 0 &&
-      !reduceOnly &&
-      HMXLib.abs(sizeDelta) > tradeSizeLimitByMarket[marketIndex]
-    ) {
-      if (isRevert) revert LimitTradeHelper_MarketMaxTradeSize();
-      else return false;
-    }
-
-    // Check trade size limit as per asset class
-    if (tradeSizeLimit[_assetClass] > 0 && !reduceOnly && HMXLib.abs(sizeDelta) > tradeSizeLimit[_assetClass]) {
+    if (tradeSizeLimitOf[marketIndex] > 0 && !reduceOnly && HMXLib.abs(sizeDelta) > tradeSizeLimitOf[marketIndex]) {
       if (isRevert) revert LimitTradeHelper_MaxTradeSize();
       else return false;
     }
 
     // Check position size limit as per market
     if (
-      positionSizeLimitByMarket[marketIndex] > 0 &&
+      positionSizeLimitOf[marketIndex] > 0 &&
       !reduceOnly &&
-      HMXLib.abs(_positionSizeE30 + sizeDelta) > positionSizeLimitByMarket[marketIndex]
+      HMXLib.abs(_positionSizeE30 + sizeDelta) > positionSizeLimitOf[marketIndex]
     ) {
-      if (isRevert) revert LimitTradeHelper_MarketMaxPositionSize();
+      if (isRevert) revert LimitTradeHelper_MaxPositionSize();
       else return false;
     }
 
-    // Check position size limit as per asset class
-    if (positionSizeLimit[_assetClass] > 0 && !reduceOnly) {
-      if (HMXLib.abs(_positionSizeE30 + sizeDelta) > positionSizeLimit[_assetClass]) {
-        if (isRevert) revert LimitTradeHelper_MaxPositionSize();
-        else return false;
-      }
-    }
     return true;
   }
 
-  function setLimitByMarketIndex(
+  function setLimit(
     uint256[] calldata _marketIndexes,
     uint256[] calldata _positionSizeLimits,
     uint256[] calldata _tradeSizeLimits
@@ -91,24 +69,14 @@ contract LimitTradeHelper is Ownable {
     );
     uint256 _len = _marketIndexes.length;
     for (uint256 i = 0; i < _len; ) {
-      positionSizeLimitByMarket[_marketIndexes[i]] = _positionSizeLimits[i];
-      tradeSizeLimitByMarket[_marketIndexes[i]] = _tradeSizeLimits[i];
+      positionSizeLimitOf[_marketIndexes[i]] = _positionSizeLimits[i];
+      tradeSizeLimitOf[_marketIndexes[i]] = _tradeSizeLimits[i];
 
-      emit LogSetTradeLimitByMarket(_marketIndexes[i], _positionSizeLimits[i], _tradeSizeLimits[i]);
+      emit LogSetLimit(_marketIndexes[i], _positionSizeLimits[i], _tradeSizeLimits[i]);
 
       unchecked {
         ++i;
       }
     }
-  }
-
-  function setPositionSizeLimit(
-    uint8 _assetClass,
-    uint256 _positionSizeLimit,
-    uint256 _tradeSizeLimit
-  ) external onlyOwner {
-    emit LogSetPositionSizeLimit(_assetClass, _positionSizeLimit, _tradeSizeLimit);
-    positionSizeLimit[_assetClass] = _positionSizeLimit;
-    tradeSizeLimit[_assetClass] = _tradeSizeLimit;
   }
 }

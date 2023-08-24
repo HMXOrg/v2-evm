e356dcd8dc5d185c61d591a7a730311f044c16a1
shutsuwei <shu.tsu.wei88@gmail.com>
2023-08-24 16:40:02 +0700

    release: rebalance HLP service

--- script/ts/configs/VaultStorage/set-service-executor-batch.ts
+++ script/ts/configs/VaultStorage/set-service-executor-batch.ts
@@ -8,7 +8,7 @@ async function main(chainId: number) {
   const config = loadConfig(chainId);
   const inputs = [
     {
-      executorAddress: config.handlers.bot,
+      executorAddress: config.services.rebalanceHLP,
       isServiceExecutor: true,
     },
   ];

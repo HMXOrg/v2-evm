4a27281ab9d9976246dd65a19f148a102569121e
Asaki-Katsu <boom@100x.fi>
2023-08-30 16:34:15 +0700

    chore: mock interface for old handler

---
+++ src/handlers/interfaces/ICrossMarginHandler.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import { CrossMarginService } from "@hmx/services/CrossMarginService.sol";
+
+interface ICrossMarginHandler {
+  /**
+   * Errors
+   */
+  error ICrossMarginHandler_InvalidAddress();
+  error ICrossMarginHandler_MismatchMsgValue();
+  error ICrossMarginHandler_InCorrectValueTransfer();
+  error ICrossMarginHandler_NotWhitelisted();
+  error ICrossMarginHandler_InsufficientExecutionFee();
+  error ICrossMarginHandler_NoOrder();
+  error ICrossMarginHandler_NotOrderOwner();
+  error ICrossMarginHandler_NotExecutionState();
+  error ICrossMarginHandler_NotWNativeToken();
+  error ICrossMarginHandler_Unauthorized();
+  error ICrossMarginHandler_BadAmount();
+
+  /**
+   * Structs
+   */
+  enum WithdrawOrderStatus {
+    PENDING,
+    SUCCESS,
+    FAIL
+  }
+
+  struct WithdrawOrder {
+    uint256 orderId;
+    uint256 amount;
+    uint256 executionFee;
+    uint48 createdTimestamp;
+    uint48 executedTimestamp;
+    address payable account;
+    address token;
+    CrossMarginService crossMarginService;
+    uint8 subAccountId;
+    WithdrawOrderStatus status; // 0 = pending, 1 = execution success, 2 = execution fail
+    bool shouldUnwrap;
+  }
+
+  /**
+   * States
+   */
+
+  function nextExecutionOrderIndex() external view returns (uint256);
+
+  function crossMarginService() external returns (address);
+
+  function pyth() external returns (address);
+
+  /**
+   * Functions
+   */
+
+  function depositCollateral(uint8 _subAccountId, address _token, uint256 _amount, bool _shouldWrap) external payable;
+
+  function createWithdrawCollateralOrder(
+    uint8 _subAccountId,
+    address _token,
+    uint256 _amount,
+    uint256 _executionFee,
+    bool _shouldUnwrap
+  ) external payable returns (uint256 _orderId);
+
+  function executeOrder(
+    uint256 _endIndex,
+    address payable _feeReceiver,
+    bytes32[] memory _priceData,
+    bytes32[] memory _publishTimeData,
+    uint256 _minPublishTime,
+    bytes32 _encodedVaas
+  ) external;
+
+  function setCrossMarginService(address _address) external;
+
+  function setPyth(address _address) external;
+
+  function setOrderExecutor(address _executor, bool _isAllow) external;
+
+  function setMinExecutionFee(uint256 _newMinExecutionFee) external;
+
+  function setMaxExecutionChuck(uint256 _maxExecutionChuck) external;
+
+  function getWithdrawOrders() external view returns (WithdrawOrder[] memory _withdrawOrder);
+
+  function getWithdrawOrderLength() external view returns (uint256);
+
+  function getActiveWithdrawOrders(
+    uint256 _limit,
+    uint256 _offset
+  ) external view returns (WithdrawOrder[] memory _withdrawOrder);
+
+  function getExecutedWithdrawOrders(
+    address _account,
+    uint256 _limit,
+    uint256 _offset
+  ) external view returns (WithdrawOrder[] memory _withdrawOrder);
+}

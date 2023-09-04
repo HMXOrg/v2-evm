899f8ded1090e488d8dad28887e3a811d81549ad
kangdaiyu <kangdaiyu88@gmail.com>
2023-09-04 11:12:21 +0700

    fix: config

--- script/ts/deploys/PositionReader/index.ts
+++ script/ts/deploys/PositionReader/index.ts
@@ -9,14 +9,10 @@ async function main() {
 
   const Contract = await ethers.getContractFactory("PositionReader", deployer);
   const contract = await Contract.deploy(
-    "0x439860Bea87C4De1757e30Cb4073578CE450Cbbd",
-    "0x58120a4f1959D1670e84BBBc1C403e08cD152bae",
-    "0x0DbdfF9CeE7cA35C8bcd48B031f8C2aB274F4552",
-    "0xCd6a5d5D7028D3EF48e25B626180b256EF901C4a"
-    // config.storages.config,
-    // config.storages.perp,
-    // config.oracles.middleware,
-    // config.calculator
+    config.storages.config,
+    config.storages.perp,
+    config.oracles.middleware,
+    config.calculator
   );
   await contract.deployed();
   console.log(`Deploying PositionReader Contract`);

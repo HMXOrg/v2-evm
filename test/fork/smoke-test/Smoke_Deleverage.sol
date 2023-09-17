fe2fe77a711822d71d27dacc3216c637c34d6db3
Asaki-Katsu <boom@100x.fi>
2023-09-17 21:05:44 +0700

    test: fix bug on deleverage

--- test/fork/smoke-test/Smoke_Deleverage.sol
+++ test/fork/smoke-test/Smoke_Deleverage.sol
@@ -14,8 +14,14 @@ import { PythStructs } from "pyth-sdk-solidity/IPyth.sol";
 import { HMXLib } from "@hmx/libraries/HMXLib.sol";
 
 import "forge-std/console.sol";
+import "forge-std/console2.sol";
 
 contract Smoke_Liquidate is Smoke_Base {
+  error Smoke_Liquidate_NoPosition();
+  error Smoke_Liquidate_NoFilteredPosition();
+
+  IPerpStorage.Position[] internal filteredPositions;
+
   function setUp() public virtual override {
     super.setUp();
 
@@ -35,7 +41,11 @@ contract Smoke_Liquidate is Smoke_Base {
     );
   }
 
-  function testCorrectness_SmokeTest_deleverageFirstTen() external {
+  function testBOOM_TEST() external {
+    console2.logBytes(abi.encodeWithSignature("IVaultStorage_NotWhiteListed()"));
+  }
+
+  function testCorrectness_SmokeTest_deleverage() external {
     IEcoPythCalldataBuilder.BuildData[] memory data = _buildDataForPrice_Deleverage();
     (
       uint256 _minPublishTime,
@@ -43,15 +53,24 @@ contract Smoke_Liquidate is Smoke_Base {
       bytes32[] memory _publishTimeUpdateCalldata
     ) = ecoPythBuilder.build(data);
 
-    IPerpStorage.Position[] memory positions = perpStorage.getActivePositions(10, 0);
+    IPerpStorage.Position[] memory positions = perpStorage.getActivePositions(3, 0);
 
     if (positions.length == 0) {
-      console.log("No position to be deleveraged");
-      return;
+      revert Smoke_Liquidate_NoPosition();
     }
 
-    vm.startPrank(POS_MANAGER);
-    botHandler.updateLiquidityEnabled(false);
+    vm.startPrank(address(tradeService));
+    vaultStorage.removeHlpLiquidityDebtUSDE30(vaultStorage.hlpLiquidityDebtUSDE30());
+    vm.stopPrank();
+
+    vm.prank(address(botHandler));
+    ecoPyth.updatePriceFeeds(
+      _priceUpdateCalldata,
+      _publishTimeUpdateCalldata,
+      _minPublishTime,
+      keccak256("someEncodedVaas")
+    );
+
     for (uint i = 0; i < positions.length; i++) {
       if (
         positions[i].primaryAccount == address(0) ||
@@ -60,16 +79,30 @@ contract Smoke_Liquidate is Smoke_Base {
           positions[i].subAccountId,
           positions[i].marketIndex,
           positions[i].avgEntryPriceE30
-        )
-      ) continue; // if address(0), ignore.
+        ) ||
+        positions[i].marketIndex != 1 // NOTE on test, focus for ETH pos only, avoid price conflict
+      ) continue;
+      filteredPositions.push(positions[i]);
 
-      address subAccount = HMXLib.getSubAccount(positions[i].primaryAccount, positions[i].subAccountId);
-      bytes32 positionId = HMXLib.getPositionId(subAccount, positions[i].marketIndex);
+      vm.startPrank(address(tradeService));
+      vaultStorage.removeHlpLiquidityDebtUSDE30(vaultStorage.hlpLiquidityDebtUSDE30());
+      vm.stopPrank();
+    }
+
+    if (positions.length == 0) {
+      revert Smoke_Liquidate_NoFilteredPosition();
+    }
+
+    vm.startPrank(POS_MANAGER);
+    botHandler.updateLiquidityEnabled(false);
+    for (uint i = 0; i < filteredPositions.length; i++) {
+      address subAccount = HMXLib.getSubAccount(filteredPositions[i].primaryAccount, filteredPositions[i].subAccountId);
+      bytes32 positionId = HMXLib.getPositionId(subAccount, filteredPositions[i].marketIndex);
 
       botHandler.deleverage(
-        positions[i].primaryAccount,
-        positions[i].subAccountId,
-        positions[i].marketIndex,
+        filteredPositions[i].primaryAccount,
+        filteredPositions[i].subAccountId,
+        filteredPositions[i].marketIndex,
         USDC,
         _priceUpdateCalldata,
         _publishTimeUpdateCalldata,
@@ -95,7 +128,8 @@ contract Smoke_Liquidate is Smoke_Base {
       data[i - 1].publishTime = uint160(block.timestamp);
       data[i - 1].maxDiffBps = 20_000;
       if (i == 1) {
-        data[i - 1].priceE8 = _ecoPythPrice.price * 2;
+        // ETH
+        data[i - 1].priceE8 = (_ecoPythPrice.price * 15) / 10;
       } else {
         data[i - 1].priceE8 = _ecoPythPrice.price;
       }

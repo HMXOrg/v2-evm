71a64e295995993982ae6affb767ece57bdd263a
Jonathan Xina <john.xina.p88@gmail.com>
2023-09-28 14:47:15 +0700

    feat: orderbook oracle

--- src/oracles/OrderbookOracle.sol
+++ src/oracles/OrderbookOracle.sol
@@ -8,21 +8,23 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import { TickMath } from "@aperture-finance/uni-v3-lib/TickMath.sol";
 
-contract OrderbookDepthOracle is Ownable {
+contract OrderbookOracle is Ownable {
   using SafeCast for uint256;
   using SafeCast for int256;
 
   // errors
-  error OrderbookDepthOracle_OnlyUpdater();
-  error OrderbookDepthOracle_InvalidArgs();
-  error OrderbookDepthOracle_MarketIndexHasAlreadyBeenDefined();
-  error OrderbookDepthOracle_PriceFeedNotFound();
+  error OrderbookOracle_OnlyUpdater();
+  error OrderbookOracle_InvalidArgs();
+  error OrderbookOracle_MarketIndexHasAlreadyBeenDefined();
+  error OrderbookOracle_PriceFeedNotFound();
 
   // array of 1% orderbook depth data from CEX
   // it is stored as `tick` from the Uniswap tick price math
   // https://docs.uniswap.org/contracts/v3/reference/core/libraries/TickMath
   // the order of the data will be according to market index
-  bytes32[] public depths;
+  bytes32[] public askDepths;
+  bytes32[] public bidDepths;
+  bytes32[] public coeffVariants; // C (coefficient variant) = sd / averagePrice
   // map Asset Id to index in the `prices` which is the array of tick price
   mapping(uint256 => uint256) public mapMarketIndexToIndex;
   uint256[] public marketIndexes;
@@ -45,7 +47,7 @@ contract OrderbookDepthOracle is Ownable {
    */
   modifier onlyUpdater() {
     if (!isUpdaters[msg.sender]) {
-      revert OrderbookDepthOracle_OnlyUpdater();
+      revert OrderbookOracle_OnlyUpdater();
     }
     _;
   }
@@ -61,18 +63,34 @@ contract OrderbookDepthOracle is Ownable {
     return marketIndexes;
   }
 
-  function updateDepths(bytes32[] calldata _depths) external onlyUpdater {
-    depths = _depths;
+  function updateData(
+    bytes32[] calldata _askDepths,
+    bytes32[] calldata _bidDepths,
+    bytes32[] calldata _coeffVariants
+  ) external onlyUpdater {
+    askDepths = _askDepths;
+    bidDepths = _bidDepths;
+    coeffVariants = _coeffVariants;
   }
 
-  function getDepth(uint256 marketIndex) external view returns (uint256 depth) {
-    if (mapMarketIndexToIndex[marketIndex] == 0) revert OrderbookDepthOracle_PriceFeedNotFound();
+  function getData(
+    uint256 marketIndex
+  ) external view returns (uint256 askDepth, uint256 bidDepth, uint256 coeffVariant) {
+    if (mapMarketIndexToIndex[marketIndex] == 0) revert OrderbookOracle_PriceFeedNotFound();
     uint256 index = mapMarketIndexToIndex[marketIndex] - 1;
     uint256 internalIndex = index % 10;
-    uint256 word = uint256(depths[index / 10]);
-    int24 tick = int24(int256((word >> (256 - (24 * (internalIndex + 1))))));
-    uint160 sqrtPriceX96 = TickMath.getSqrtRatioAtTick(tick);
-    depth = (uint256(sqrtPriceX96) * (uint256(sqrtPriceX96)) * (1e8)) >> (96 * 2);
+
+    int24 askDepthTick = int24(int256((uint256(askDepths[index / 10]) >> (256 - (24 * (internalIndex + 1))))));
+    uint160 sqrtAskDepthX96 = TickMath.getSqrtRatioAtTick(askDepthTick);
+    askDepth = (uint256(sqrtAskDepthX96) * (uint256(sqrtAskDepthX96)) * (1e8)) >> (96 * 2);
+
+    int24 bidDepthTick = int24(int256((uint256(bidDepths[index / 10]) >> (256 - (24 * (internalIndex + 1))))));
+    uint160 sqrtBidDepthX96 = TickMath.getSqrtRatioAtTick(bidDepthTick);
+    bidDepth = (uint256(sqrtBidDepthX96) * (uint256(sqrtBidDepthX96)) * (1e8)) >> (96 * 2);
+
+    int24 coeffVariantTick = int24(int256((uint256(coeffVariants[index / 10]) >> (256 - (24 * (internalIndex + 1))))));
+    uint160 sqrtCoeffVariantX96 = TickMath.getSqrtRatioAtTick(coeffVariantTick);
+    coeffVariant = (uint256(sqrtCoeffVariantX96) * (uint256(sqrtCoeffVariantX96)) * (1e8)) >> (96 * 2);
   }
 
   /// @dev Sets the `isActive` status of the given account as a price updater.
@@ -87,7 +105,7 @@ contract OrderbookDepthOracle is Ownable {
   }
 
   function setUpdaters(address[] calldata _accounts, bool[] calldata _isActives) external onlyOwner {
-    if (_accounts.length != _isActives.length) revert OrderbookDepthOracle_InvalidArgs();
+    if (_accounts.length != _isActives.length) revert OrderbookOracle_InvalidArgs();
     for (uint256 i = 0; i < _accounts.length; ) {
       // Set the `isActive` status of the given account
       isUpdaters[_accounts[i]] = _isActives[i];
@@ -116,14 +134,14 @@ contract OrderbookDepthOracle is Ownable {
   }
 
   function _insertMarketIndex(uint256 _marketIndex) internal {
-    if (mapMarketIndexToIndex[_marketIndex] != 0) revert OrderbookDepthOracle_MarketIndexHasAlreadyBeenDefined();
+    if (mapMarketIndexToIndex[_marketIndex] != 0) revert OrderbookOracle_MarketIndexHasAlreadyBeenDefined();
     mapMarketIndexToIndex[_marketIndex] = indexCount;
     emit SetMarketIndex(indexCount, _marketIndex);
     marketIndexes.push(_marketIndex);
     ++indexCount;
   }
 
-  function buildPriceUpdateData(int24[] calldata _depths) external pure returns (bytes32[] memory _updateData) {
+  function buildUpdateData(int24[] calldata _depths) external pure returns (bytes32[] memory _updateData) {
     _updateData = new bytes32[]((_depths.length + MAX_DEPTH_PER_WORD - 1) / MAX_DEPTH_PER_WORD);
     for (uint256 i; i < _depths.length; ++i) {
       uint256 outerIndex = i / MAX_DEPTH_PER_WORD;

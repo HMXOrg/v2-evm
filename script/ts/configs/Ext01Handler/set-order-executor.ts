5018ba8b224a8d704ae7fb467218b4d4796be161
Jonathan Xina <john.xina.p88@gmail.com>
2023-10-05 17:54:02 +0700

    deploy: new ext01 handler

--- script/ts/configs/Ext01Handler/set-order-executor.ts
+++ script/ts/configs/Ext01Handler/set-order-executor.ts
@@ -15,7 +15,7 @@ async function main(chainId: number) {
   const safeWrapper = new SafeWrapper(chainId, config.safe, deployer);
   const ext01Handler = Ext01Handler__factory.connect(config.handlers.ext01, deployer);
 
-  const orderExecutor = "0x6629eC35c8Aa279BA45Dbfb575c728d3812aE31a";
+  const orderExecutor = "0x0578c797798ae89b688cd5676348344d7d0ec35e";
   const isAllow = true;
 
   console.log("[config/Ext01Handler] Ext01Handler setOrderExecutor...");

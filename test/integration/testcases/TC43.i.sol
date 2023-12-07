d688232f911a96fa14a79c5c2baeaa76b84b9694
Jonathan Xina <john.xina.p88@gmail.com>
2023-12-07 14:56:51 +0700

    feat: add test where execution fee is not enough

--- test/integration/testcases/TC43.i.sol
+++ test/integration/testcases/TC43.i.sol
@@ -359,7 +359,7 @@ contract TC43 is BaseIntTest_WithActions {
     assertEq(vaultStorage.traderBalances(BOB, address(usdc)), 99899.8 * 1e6);
   }
 
-  function testCorrectness_TC43_intentHandler_payExecutionFeeWithDai() external {
+  function testCorrectness_TC43_intentHandler_payExecutionFeeWithMultipleToken() external {
     uint256 privateKey = uint256(keccak256(bytes("1")));
     BOB = vm.addr(privateKey);
 
@@ -391,10 +391,13 @@ contract TC43 is BaseIntTest_WithActions {
     {
       assertVaultTokenBalance(address(usdc), 0, "TC43: before deposit collateral");
     }
-    dai.mint(BOB, 100_000 * 1e18);
-    depositCollateral(BOB, 0, ERC20(address(dai)), 100_000 * 1e18);
+    dai.mint(BOB, 0.1 * 1e18);
+    depositCollateral(BOB, 0, ERC20(address(dai)), 0.1 * 1e18);
+    wbtc.mint(BOB, 100 * 1e8);
+    depositCollateral(BOB, 0, ERC20(address(wbtc)), 1 * 1e8);
     {
-      assertVaultTokenBalance(address(dai), 100_000 * 1e18, "TC43: after deposit collateral");
+      assertVaultTokenBalance(address(dai), 0.1 * 1e18, "TC43: after deposit collateral");
+      assertVaultTokenBalance(address(wbtc), 101 * 1e8, "TC43: after deposit collateral");
     }
 
     // Long ETH
@@ -436,9 +439,94 @@ contract TC43 is BaseIntTest_WithActions {
 
     assertEq(perpStorage.getNumberOfSubAccountPosition(BOB), 1);
 
-    // Initial collateral = 100,000 usdc
-    // 1st trade's trading fee = 100000 - 100 = 99900
-    // 1st trade's execution fee = 99900 - 0.1 = 99899.9
-    assertEq(vaultStorage.traderBalances(BOB, address(dai)), 99899.9 * 1e18);
+    // Initial collateral = 0.1 DAI
+    //                    = 1 WBTC (WBTC @ $19,998.3457779) = $19,998.3457779
+    // 1st trade's execution fee = 0.1 DAI - 0.1 = 0 DAI
+    // 1st trade's trading fee = 19998.3457779 - 100 = 19898.3457779 => 19898.3457779/19,998.3457779 = 0.99499959 WBTC
+
+    assertEq(vaultStorage.traderBalances(BOB, address(dai)), 0 * 1e18);
+    assertEq(vaultStorage.traderBalances(BOB, address(wbtc)), 0.99499959 * 1e8);
+  }
+
+  function testCorrectness_TC43_intentHandler_notEnoughCollateralForExecutionFee() external {
+    uint256 privateKey = uint256(keccak256(bytes("1")));
+    BOB = vm.addr(privateKey);
+
+    // T0: Initialized state
+    // ALICE as liquidity provider
+    // BOB as trader
+    IConfigStorage.MarketConfig memory _marketConfig = configStorage.getMarketConfigByIndex(wbtcMarketIndex);
+
+    _marketConfig.maxLongPositionSize = 20_000_000 * 1e30;
+    _marketConfig.maxShortPositionSize = 20_000_000 * 1e30;
+    configStorage.setMarketConfig(wbtcMarketIndex, _marketConfig, false);
+
+    // T1: Add liquidity in pool USDC 100_000 , WBTC 100
+    vm.deal(ALICE, executionOrderFee);
+    wbtc.mint(ALICE, 100 * 1e8);
+
+    addLiquidity(
+      ALICE,
+      ERC20(address(wbtc)),
+      100 * 1e8,
+      executionOrderFee,
+      tickPrices,
+      publishTimeDiff,
+      block.timestamp,
+      true
+    );
+
+    // T2: Create market order
+    {
+      assertVaultTokenBalance(address(usdc), 0, "TC43: before deposit collateral");
+    }
+    dai.mint(BOB, 1205 * 1e18);
+    depositCollateral(BOB, 0, ERC20(address(dai)), 1205 * 1e18);
+
+    // Long ETH
+    vm.deal(BOB, 1 ether);
+
+    {
+      // before create order, must be empty
+      assertEq(limitTradeHandler.limitOrdersIndex(getSubAccount(BOB, 0)), 0);
+      assertEq(BOB.balance, 1 ether);
+    }
+
+    // Bob will open two positions on ETH and BTC markets
+    IIntentHandler.ExecuteIntentInputs memory executeIntentInputs;
+    executeIntentInputs.accountAndSubAccountIds = new bytes32[](1);
+    executeIntentInputs.accountAndSubAccountIds[0] = intentBuilder.buildAccountAndSubAccountId(BOB, 0);
+
+    executeIntentInputs.cmds = new bytes32[](1);
+    executeIntentInputs.cmds[0] = intentBuilder.buildTradeOrder(
+      wethMarketIndex, // marketIndex
+      100_000 * 1e30, // sizeDelta
+      0, // triggerPrice
+      4000 * 1e30, // acceptablePrice
+      true, // triggerAboveThreshold
+      false, // reduceOnly
+      address(usdc), // tpToken
+      block.timestamp + 5 minutes // minPublishTime
+    );
+
+    (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, executeIntentInputs.cmds[0]);
+    executeIntentInputs.signatures = new bytes[](1);
+    executeIntentInputs.signatures[0] = abi.encodePacked(r, s, v);
+
+    executeIntentInputs.priceData = pyth.buildPriceUpdateData(tickPrices);
+    executeIntentInputs.publishTimeData = pyth.buildPublishTimeUpdateData(publishTimeDiff);
+    executeIntentInputs.minPublishTime = block.timestamp;
+    executeIntentInputs.encodedVaas = keccak256("someEncodedVaas");
+
+    intentHandler.execute(executeIntentInputs);
+
+    // The order failed to executed here because collateral is short by 0.1 usd for the exection fee.
+    // Therefore, no position will be opened.
+    assertEq(perpStorage.getNumberOfSubAccountPosition(BOB), 0);
+
+    // Initial collateral = 1205 DAI
+    // 1st trade's execution fee = 1205 - 0.1 = 1204.9 DAI
+    // execution fee is deducted even if the order failed
+    assertEq(vaultStorage.traderBalances(BOB, address(dai)), 1204.9 * 1e18);
   }
 }

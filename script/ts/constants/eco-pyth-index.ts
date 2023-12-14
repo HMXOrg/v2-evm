699c5e7ade43751779d1753c870b01649bdbb7b1
Jonathan Xina <john.xina.p88@gmail.com>
2023-12-14 12:11:05 +0700

    chore: remove SHIBUSD and PEPEUSD

--- script/ts/constants/eco-pyth-index.ts
+++ script/ts/constants/eco-pyth-index.ts
@@ -51,11 +51,9 @@ export const ecoPythPriceFeedIdsByIndex = [
   "0x09f7c1d7dfbb7df2b8fe3d3d87ee94a2259d212da4f30c1f0540d066dfa44723", // TIAUSD
   "0x93da3352f9f1d105fdfe4971cfa80e9dd777bfc5d0f683ebb6e1294b92137bb7", // AVAXUSD
   "0x7a5bc1d2b56ad029048cd63964b3ad2776eadf812edc1a43a31406cb54bff592", // INJUSD
-  "0xf0d57deca57b3da2fe63a493f4c25925fdfd8edf834b20f93e1f84dbd1504d4a", // SHIBUSD
   "0xca3eed9b267293f6595901c734c7525ce8ef49adafe8284606ceb307afa2ca5b", // DOTUSD
   "0x53614f1cb0c031d4af66c04cb9c756234adad0e1cee85303795091499a4084eb", // SEIUSD
   "0xb00b60f88b03a6a625a8d1c048c3f66653edf217439983d037e7222c4e612819", // ATOMUSD
-  "0xd69731a2e74ac1ce884fc3890f7ee324b6deb66147055249568869ed700882e4", // PEPEUSD
 ];
 export const ecoPythAssetIdByIndex = [
   "0x4554480000000000000000000000000000000000000000000000000000000000", // ETH
@@ -107,11 +105,9 @@ export const ecoPythAssetIdByIndex = [
   ethers.utils.formatBytes32String("TIA"),
   ethers.utils.formatBytes32String("AVAX"),
   ethers.utils.formatBytes32String("INJ"),
-  ethers.utils.formatBytes32String("SHIB"),
   ethers.utils.formatBytes32String("DOT"),
   ethers.utils.formatBytes32String("SEI"),
   ethers.utils.formatBytes32String("ATOM"),
-  ethers.utils.formatBytes32String("PEPE"),
 ];
 export const ecoPythHoomanReadableByIndex = [
   "ETH",
@@ -163,9 +159,7 @@ export const ecoPythHoomanReadableByIndex = [
   "TIA",
   "AVAX",
   "INJ",
-  "SHIB",
   "DOT",
   "SEI",
   "ATOM",
-  "PEPE",
 ];

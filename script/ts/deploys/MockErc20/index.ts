6c055ad325e49e1e2d991ab211245200d8bb35bb
Jonathan Xina <john.xina.p88@gmail.com>
2023-12-18 13:12:28 +0700

    feat: mock erc20 deployment

---
+++ script/ts/deploys/MockErc20/index.ts
@@ -0,0 +1,25 @@
+import { ethers, tenderly } from "hardhat";
+import { getConfig, writeConfigFile } from "../../utils/config";
+import { MockErc20__factory } from "../../../../typechain";
+
+const config = getConfig();
+
+async function main() {
+  const deployer = (await ethers.getSigners())[0];
+
+  console.log(`Deploying MockErc20 Contract`);
+  const MockErc20 = new MockErc20__factory(deployer);
+  const mockErc20 = await MockErc20.deploy("USDC Circle", "USDC", 6);
+  await mockErc20.deployed();
+  console.log(`Deployed at: ${mockErc20.address}`);
+
+  await tenderly.verify({
+    address: mockErc20.address,
+    name: "MockErc20",
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});

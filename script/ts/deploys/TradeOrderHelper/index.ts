d831c31382333c5aedaa16059f0b943f07c3802a
Jonathan Xina <john.xina.p88@gmail.com>
2024-01-17 14:44:03 +0700

    feat: fix tradeorderhelper deployment script

--- script/ts/deploys/TradeOrderHelper/index.ts
+++ script/ts/deploys/TradeOrderHelper/index.ts
@@ -1,29 +1,26 @@
-import { ethers, tenderly, upgrades, network } from "hardhat";
+import { ethers, tenderly } from "hardhat";
 import { getConfig, writeConfigFile } from "../../utils/config";
-import { getImplementationAddress } from "@openzeppelin/upgrades-core";
 
 const config = getConfig();
 
 async function main() {
   const deployer = (await ethers.getSigners())[0];
+  const contract = await ethers.deployContract(
+    "TradeOrderHelper",
+    [config.storages.config, config.storages.perp, config.oracles.middleware, config.services.trade],
+    deployer
+  );
 
-  const Contract = await ethers.getContractFactory("TradeOrderHelper", deployer);
-  const contract = await upgrades.deployProxy(Contract, [
-    config.storages.config,
-    config.storages.perp,
-    config.oracles.middleware,
-    config.services.trade,
-  ]);
   await contract.deployed();
-  console.log(`Deploying TradeOrderHelper Contract`);
-  console.log(`Deployed at: ${contract.address}`);
+  console.log(`[deploys/Dexter] Deploying TradeOrderHelper Contract`);
+  console.log(`[deploys/Dexter] Deployed at: ${contract.address}`);
 
   config.helpers.tradeOrder = contract.address;
   writeConfigFile(config);
 
   await tenderly.verify({
-    address: await getImplementationAddress(network.provider, contract.address),
-    name: "TradeHelper",
+    address: contract.address,
+    name: "TradeOrderHelper",
   });
 }
 

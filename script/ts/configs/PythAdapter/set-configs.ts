690b304dac469f54646c1334afb2f7111cf24bca
Jonathan Xina <john.xina.p88@gmail.com>
2024-01-23 17:29:09 +0700

    deploy: mantausd market

--- script/ts/configs/PythAdapter/set-configs.ts
+++ script/ts/configs/PythAdapter/set-configs.ts
@@ -7,8 +7,8 @@ import SafeWrapper from "../../wrappers/SafeWrapper";
 
 const inputs = [
   {
-    assetId: ethers.utils.formatBytes32String("ICP"),
-    pythPriceId: ethers.utils.formatBytes32String("ICP"),
+    assetId: ethers.utils.formatBytes32String("MANTA"),
+    pythPriceId: ethers.utils.formatBytes32String("MANTA"),
     inverse: false,
   },
 ];

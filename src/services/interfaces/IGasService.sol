42ad673d465df1721a8dd7cf84d8fe2805cb4b74
Jonathan Xina <john.xina.p88@gmail.com>
2024-02-27 18:05:51 +0700

    feat: add more info in events

--- src/services/interfaces/IGasService.sol
+++ src/services/interfaces/IGasService.sol
@@ -5,8 +5,17 @@ interface IGasService {
   error GasService_NotEnoughCollateral();
 
   event LogSetParams(uint256 executionFeeInUsd, address executionFeeTreasury);
-  event LogCollectExecutionFeeValue(uint256 executionFeeUsd);
-  event LogCollectExecutionFeeAmount(address token, uint256 executionFeeAmount);
+  event LogCollectExecutionFeeValue(address subAccount, uint256 marketIndex, uint256 executionFeeUsd);
+  event LogCollectExecutionFeeAmount(
+    address subAccount,
+    uint256 marketIndex,
+    address token,
+    uint256 executionFeeAmount
+  );
 
-  function collectExecutionFeeFromCollateral(address _primaryAccount, uint8 _subAccountId) external;
+  function collectExecutionFeeFromCollateral(
+    address _primaryAccount,
+    uint8 _subAccountId,
+    uint256 _marketIndex
+  ) external;
 }

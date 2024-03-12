1ed9a9789e2fd79e1b0d908bcf8b214d19285886
Jonathan Xina <john.xina.p88@gmail.com>
2024-03-12 13:49:55 +0700

    feat: deployment scripts

--- script/ts/configs/ConfigStorage/set-trade-service-hooks.ts
+++ script/ts/configs/ConfigStorage/set-trade-service-hooks.ts
@@ -1,24 +1,22 @@
 import { ConfigStorage__factory } from "../../../../typechain";
 import { loadConfig } from "../../utils/config";
-import SafeWrapper from "../../wrappers/SafeWrapper";
+import { OwnerWrapper } from "../../wrappers/OwnerWrapper";
 import { Command } from "commander";
 import signers from "../../entities/signers";
 
 async function main(chainId: number) {
   const config = loadConfig(chainId);
   const deployer = signers.deployer(chainId);
-  const safeWrapper = new SafeWrapper(chainId, deployer);
+  const ownerWrapper = new OwnerWrapper(chainId, deployer);
   const configStorage = ConfigStorage__factory.connect(config.storages.config, deployer);
 
   console.log("[config/ConfigStorage] ConfigStorage setTradeServiceHooks...");
-  const tx = await safeWrapper.proposeTransaction(
+  await ownerWrapper.authExec(
     configStorage.address,
-    0,
     configStorage.interface.encodeFunctionData("setTradeServiceHooks", [
       [config.hooks.tlc, config.hooks.tradingStaking],
     ])
   );
-  console.log(`[config/ConfigStorage] Proposed ${tx} to setTradeServiceHooks`);
 }
 
 const program = new Command();

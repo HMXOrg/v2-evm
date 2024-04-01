e356837aa5fc3faec01927c7ae1ce31a2a0639a3
Jonathan Xina <john.xina.p88@gmail.com>
2024-04-01 13:51:43 +0700

    chore: add PENDLEUSD market

--- script/ts/commands/EcoPyth/feed-price-eco-pyth.ts
+++ script/ts/commands/EcoPyth/feed-price-eco-pyth.ts
@@ -6,7 +6,7 @@ import { loadConfig } from "../../utils/config";
 import { getUpdatePriceData } from "../../utils/price";
 import signers from "../../entities/signers";
 import chains from "../../entities/chains";
-import HmxApiWrapper from "../../wrappers/HMXApiWrapper";
+import HmxApiWrapper from "../../wrappers/HMXAPIWrapper";
 import { ethers } from "ethers";
 
 async function main(chainId: number) {
@@ -16,6 +16,8 @@ async function main(chainId: number) {
   const hmxApi = new HmxApiWrapper(chainId);
 
   const pyth = EcoPyth__factory.connect(config.oracles.ecoPyth2, deployer);
+  console.log(ethers.utils.formatBytes32String("PENDLE"));
+  return;
 
   const [readableTable, minPublishedTime, priceUpdateData, publishTimeDiffUpdateData, hashedVaas] =
     await getUpdatePriceData(ecoPythPriceFeedIdsByIndex, provider);

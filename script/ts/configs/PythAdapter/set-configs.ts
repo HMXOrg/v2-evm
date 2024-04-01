ae632d18c207d54655a1d12f270273d50c1a6448
Jonathan Xina <john.xina.p88@gmail.com>
2024-04-01 15:23:34 +0700

    deploy: add PENDLEUSD market

--- script/ts/configs/PythAdapter/set-configs.ts
+++ script/ts/configs/PythAdapter/set-configs.ts
@@ -7,13 +7,8 @@ import { OwnerWrapper } from "../../wrappers/OwnerWrapper";
 
 const inputs = [
   {
-    assetId: ethers.utils.formatBytes32String("ybETH2"),
-    pythPriceId: ethers.utils.formatBytes32String("ybETH2"),
-    inverse: false,
-  },
-  {
-    assetId: ethers.utils.formatBytes32String("ybUSDB2"),
-    pythPriceId: ethers.utils.formatBytes32String("ybUSDB2"),
+    assetId: ethers.utils.formatBytes32String("PENDLE"),
+    pythPriceId: ethers.utils.formatBytes32String("PENDLE"),
     inverse: false,
   },
 ];

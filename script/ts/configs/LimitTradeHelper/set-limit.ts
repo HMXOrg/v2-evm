e1728e3d3ccd9bea882f347d16e731b9e171af98
Jonathan Xina <john.xina.p88@gmail.com>
2024-04-01 15:59:21 +0700

    feat: set limit

--- script/ts/configs/LimitTradeHelper/set-limit.ts
+++ script/ts/configs/LimitTradeHelper/set-limit.ts
@@ -1,24 +1,24 @@
 import { Command } from "commander";
 import { loadConfig, loadMarketConfig } from "../../utils/config";
 import signers from "../../entities/signers";
-import SafeWrapper from "../../wrappers/SafeWrapper";
+import { OwnerWrapper } from "../../wrappers/OwnerWrapper";
 import { LimitTradeHelper__factory } from "../../../../typechain";
 import { ethers } from "ethers";
 
 async function main(chainId: number) {
   const inputs = [
     {
-      marketIndex: 16,
-      tradeSizeLimit: ethers.utils.parseUnits("200000", 30),
-      positionSizeLimit: ethers.utils.parseUnits("200000", 30),
+      marketIndex: 51,
+      tradeSizeLimit: 15000,
+      positionSizeLimit: 15000,
     },
   ];
 
   const config = loadConfig(chainId);
   const marketConfig = loadMarketConfig(chainId);
   const deployer = signers.deployer(chainId);
-  const safeWrapper = new SafeWrapper(chainId, config.safe, deployer);
-  const limitTradeHelper = LimitTradeHelper__factory.connect(config.helpers.limitTrade, deployer);
+  const ownerWrapper = new OwnerWrapper(chainId, deployer);
+  const limitTradeHelper = LimitTradeHelper__factory.connect(config.helpers.limitTrade!, deployer);
 
   console.log(`[configs/LimitTradeHelper] Set Limit By Market Index...`);
   console.table(
@@ -26,21 +26,19 @@ async function main(chainId: number) {
       return {
         marketIndex: i.marketIndex,
         market: marketConfig.markets[i.marketIndex].name,
-        positionSizeLimit: ethers.utils.formatUnits(i.positionSizeLimit, 30),
-        tradeSizeLimit: ethers.utils.formatUnits(i.tradeSizeLimit, 30),
+        positionSizeLimit: i.positionSizeLimit,
+        tradeSizeLimit: i.tradeSizeLimit,
       };
     })
   );
-  const tx = await safeWrapper.proposeTransaction(
+  await ownerWrapper.authExec(
     limitTradeHelper.address,
-    0,
     limitTradeHelper.interface.encodeFunctionData("setLimit", [
       inputs.map((input) => input.marketIndex),
-      inputs.map((input) => input.positionSizeLimit),
-      inputs.map((input) => input.tradeSizeLimit),
+      inputs.map((input) => ethers.utils.parseUnits(input.positionSizeLimit.toString(), 30)),
+      inputs.map((input) => ethers.utils.parseUnits(input.tradeSizeLimit.toString(), 30)),
     ])
   );
-  console.log(`[configs/LimitTradeHelper] Proposed tx to set limit by market index: ${tx}`);
 }
 
 const program = new Command();

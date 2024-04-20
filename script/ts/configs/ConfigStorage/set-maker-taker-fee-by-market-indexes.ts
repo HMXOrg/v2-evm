a34386e3d9331724e6f6cae23f24bba0f7a7bcc0
shutsuwei <shu.tsu.wei88@gmail.com>
2024-04-20 07:36:08 +0400

    release: market taker fee

--- script/ts/configs/ConfigStorage/set-maker-taker-fee-by-market-indexes.ts
+++ script/ts/configs/ConfigStorage/set-maker-taker-fee-by-market-indexes.ts
@@ -8,16 +8,206 @@ import { ethers } from "ethers";
 async function main(chainId: number) {
   const config = loadConfig(chainId);
 
+  const forexFees = {
+    makerFee: 5000, // 0.005%
+    takerFee: 25000, // 0.025%
+  };
+
+  const commoditiesFees = {
+    makerFee: 35000, // 0.035%
+    takerFee: 75000, // 0.075%
+  };
+  const cryptoFees = {
+    makerFee: 35000, // 0.035%
+    takerFee: 75000, // 0.075%
+  };
+
   const inputs = [
     {
-      marketIndex: 0,
-      makerFee: (0.02 / 100) * 1e8,
-      takerFee: (0.04 / 100) * 1e8,
+      marketIndex: 0, // ETHUSD
+      makerFee: 20000, // 0.02%
+      takerFee: 40000, // 0.04%
     },
     {
-      marketIndex: 1,
-      makerFee: (0.01 / 100) * 1e8,
-      takerFee: (0.04 / 100) * 1e8,
+      marketIndex: 1, // BTCUSD
+      makerFee: 10000, // 0.01%
+      takerFee: 40000, // 0.04%
+    },
+    {
+      marketIndex: 3, // JPYUSD
+      ...forexFees,
+    },
+    {
+      marketIndex: 4, // XAUUSD
+      ...commoditiesFees,
+    },
+    {
+      marketIndex: 8, // EURUSD
+      ...forexFees,
+    },
+    {
+      marketIndex: 9, // XAGUSD
+      ...commoditiesFees,
+    },
+    {
+      marketIndex: 10, // AUDUSD
+      ...forexFees,
+    },
+    {
+      marketIndex: 11, // GBPUSD
+      ...forexFees,
+    },
+    {
+      marketIndex: 12, // ADAUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 13, // MATICUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 14, // SUIUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 15, // ARBUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 16, // OPUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 17, // LTCUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 20, // BNBUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 21, // SOLUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 23, // XRPUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 25, // LINKUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 26, // USDCHF
+      ...forexFees,
+    },
+    {
+      marketIndex: 27, // DOGEUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 28, // USDCAD
+      ...forexFees,
+    },
+    {
+      marketIndex: 29, // USDSGD
+      ...forexFees,
+    },
+    {
+      marketIndex: 30, // USDCNH
+      ...forexFees,
+    },
+    {
+      marketIndex: 31, // USDHKD
+      ...forexFees,
+    },
+    {
+      marketIndex: 32, // BCHUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 33, // MEMEUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 34, // DIX
+      ...forexFees,
+    },
+    {
+      marketIndex: 35, // JTOUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 36, // STXUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 37, // ORDIUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 38, // TIAUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 39, // AVAXUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 40, // INJUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 41, // DOTUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 42, // SEIUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 43, // ATOMUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 44, // 1000PEPEUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 45, // 1000SHIBUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 46, // USDSEK
+      ...forexFees,
+    },
+    {
+      marketIndex: 47, // ICPUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 48, // MANTAUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 49, // STRKUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 50, // PYTHUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 51, // PENDLEUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 52, // WUSD
+      ...cryptoFees,
+    },
+    {
+      marketIndex: 53, // ENAUSD
+      ...cryptoFees,
     },
   ];
 

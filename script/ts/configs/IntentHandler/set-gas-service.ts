ba1722b6537558e4a21a2fbf0d35d57023d97069
Jonathan Xina <john.xina.p88@gmail.com>
2024-05-29 15:11:58 +0700

    deploy: collect gas as actual gas used

---
+++ script/ts/configs/IntentHandler/set-gas-service.ts
@@ -0,0 +1,33 @@
+import { IntentHandler__factory } from "../../../../typechain";
+import { loadConfig } from "../../utils/config";
+import { Command } from "commander";
+import signers from "../../entities/signers";
+import { OwnerWrapper } from "../../wrappers/OwnerWrapper";
+
+async function main(chainId: number) {
+  const config = loadConfig(chainId);
+  const deployer = signers.deployer(chainId);
+  const ownerWrapper = new OwnerWrapper(chainId, deployer);
+
+  const intentHandler = IntentHandler__factory.connect(config.handlers.intent, deployer);
+  console.log(`[configs/IntentHandler] Set Gas Service`);
+  await intentHandler.setGasService(config.services.gas);
+  console.log("[configs/IntentHandler] Finished");
+}
+
+const prog = new Command();
+
+prog.requiredOption("--chain-id <number>", "chain id", parseInt);
+
+prog.parse(process.argv);
+
+const opts = prog.opts();
+
+main(opts.chainId)
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });

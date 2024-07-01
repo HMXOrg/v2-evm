993a732f58b37aef08d480a668ae7c488ccb4a42
Jonathan Xina <john.xina.p88@gmail.com>
2024-07-01 14:46:45 +0700

    feat: improve set market config

--- script/ts/configs/ConfigStorage/set-market-config.ts
+++ script/ts/configs/ConfigStorage/set-market-config.ts
@@ -5,25 +5,24 @@ import { Command } from "commander";
 import signers from "../../entities/signers";
 import assetClasses from "../../entities/asset-classes";
 import SafeWrapper from "../../wrappers/SafeWrapper";
+import * as readlineSync from "readline-sync";
 
 type AddMarketConfig = {
   marketIndex: number;
-  assetId: string;
-  increasePositionFeeRateBPS: number;
-  decreasePositionFeeRateBPS: number;
-  initialMarginFractionBPS: number;
-  maintenanceMarginFractionBPS: number;
-  maxProfitRateBPS: number;
-  assetClass: number;
-  allowIncreasePosition: boolean;
-  active: boolean;
-  fundingRate: {
-    maxSkewScaleUSD: ethers.BigNumber;
-    maxFundingRate: ethers.BigNumber;
+  increasePositionFeeRateBPS?: number | undefined;
+  decreasePositionFeeRateBPS?: number | undefined;
+  initialMarginFractionBPS?: number | undefined;
+  maintenanceMarginFractionBPS?: number | undefined;
+  maxProfitRateBPS?: number | undefined;
+  allowIncreasePosition?: boolean | undefined;
+  active?: boolean | undefined;
+  fundingRate?: {
+    maxSkewScaleUSD?: ethers.BigNumber | undefined;
+    maxFundingRate?: ethers.BigNumber | undefined;
   };
-  maxLongPositionSize: ethers.BigNumber;
-  maxShortPositionSize: ethers.BigNumber;
-  isAdaptiveFeeEnabled: boolean;
+  maxLongPositionSize?: ethers.BigNumber | undefined;
+  maxShortPositionSize?: ethers.BigNumber | undefined;
+  isAdaptiveFeeEnabled?: boolean | undefined;
 };
 
 async function main(chainId: number) {
@@ -31,215 +30,10 @@ async function main(chainId: number) {
   const deployer = signers.deployer(chainId);
   const BigNumber = ethers.BigNumber;
 
-  const marketConfigs: Array<AddMarketConfig> = [
+  const inputMarketConfigs: Array<AddMarketConfig> = [
     {
       marketIndex: 3,
-      assetId: ethers.utils.formatBytes32String("JPY"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 500000,
-      assetClass: 2,
       allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
-    },
-    {
-      marketIndex: 8,
-      assetId: ethers.utils.formatBytes32String("EUR"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 500000,
-      assetClass: 2,
-      allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
-    },
-    {
-      marketIndex: 10,
-      assetId: ethers.utils.formatBytes32String("AUD"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 500000,
-      assetClass: 2,
-      allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
-    },
-    {
-      marketIndex: 11,
-      assetId: ethers.utils.formatBytes32String("GBP"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 500000,
-      assetClass: 2,
-      allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
-    },
-    {
-      marketIndex: 26,
-      assetId: ethers.utils.formatBytes32String("CHF"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 500000,
-      assetClass: 2,
-      allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
-    },
-    {
-      marketIndex: 28,
-      assetId: ethers.utils.formatBytes32String("CAD"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 500000,
-      assetClass: 2,
-      allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
-    },
-    {
-      marketIndex: 29,
-      assetId: ethers.utils.formatBytes32String("SGD"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 500000,
-      assetClass: 2,
-      allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
-    },
-    {
-      marketIndex: 30,
-      assetId: ethers.utils.formatBytes32String("CNH"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 500000,
-      assetClass: 2,
-      allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
-    },
-    {
-      marketIndex: 31,
-      assetId: ethers.utils.formatBytes32String("HKD"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 500000,
-      assetClass: 2,
-      allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
-    },
-    {
-      marketIndex: 34,
-      assetId: ethers.utils.formatBytes32String("DIX"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 250000,
-      assetClass: 2,
-      allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
-    },
-    {
-      marketIndex: 46,
-      assetId: ethers.utils.formatBytes32String("SEK"),
-      maxLongPositionSize: ethers.utils.parseUnits("1000000", 30),
-      maxShortPositionSize: ethers.utils.parseUnits("1000000", 30),
-      increasePositionFeeRateBPS: 1,
-      decreasePositionFeeRateBPS: 1,
-      initialMarginFractionBPS: 10,
-      maintenanceMarginFractionBPS: 5,
-      maxProfitRateBPS: 500000,
-      assetClass: 2,
-      allowIncreasePosition: true,
-      active: true,
-      fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("10000000", 30),
-        maxFundingRate: ethers.utils.parseUnits("1", 18),
-      },
-      isAdaptiveFeeEnabled: false,
     },
   ];
 
@@ -247,33 +41,92 @@ async function main(chainId: number) {
   const tradeHelper = TradeHelper__factory.connect(config.helpers.trade, deployer);
   const safeWrapper = new SafeWrapper(chainId, config.safe, deployer);
 
-  console.log("[ConfigStorage] Setting market config...");
-  for (let i = 0; i < marketConfigs.length; i++) {
-    console.log(
-      `[ConfigStorage] Setting ${ethers.utils.parseBytes32String(marketConfigs[i].assetId)} market config...`
+  const currentMarketConfigs = await configStorage.getMarketConfigs();
+
+  const toBeMarketConfigs = await Promise.all(
+    inputMarketConfigs.map(async (each) => {
+      return {
+        ...currentMarketConfigs[each.marketIndex],
+        isAdaptiveFeeEnabled: await configStorage.isAdaptiveFeeEnabledByMarketIndex(each.marketIndex),
+        ...each,
+      };
+    })
+  );
+  console.log("Press Option+Z for the console text to overflow");
+  for (let i = 0; i < toBeMarketConfigs.length; i++) {
+    const each = toBeMarketConfigs[i];
+    const existingMarketConfig = currentMarketConfigs[each.marketIndex];
+    const existing = {
+      marketIndex: each.marketIndex,
+      assetId: ethers.utils.parseBytes32String(existingMarketConfig.assetId),
+      increasePositionFeeRateBPS: existingMarketConfig.increasePositionFeeRateBPS,
+      decreasePositionFeeRateBPS: existingMarketConfig.decreasePositionFeeRateBPS,
+      initialMarginFractionBPS: existingMarketConfig.initialMarginFractionBPS,
+      maintenanceMarginFractionBPS: existingMarketConfig.maintenanceMarginFractionBPS,
+      maxProfitRateBPS: existingMarketConfig.maxProfitRateBPS,
+      assetClass: existingMarketConfig.assetClass,
+      allowIncreasePosition: existingMarketConfig.allowIncreasePosition,
+      active: existingMarketConfig.active,
+      fundingRate: {
+        maxSkewScaleUSD: existingMarketConfig.fundingRate.maxSkewScaleUSD.toString(),
+        maxFundingRate: existingMarketConfig.fundingRate.maxFundingRate.toString(),
+      },
+      maxLongPositionSize: existingMarketConfig.maxLongPositionSize.toString(),
+      maxShortPositionSize: existingMarketConfig.maxShortPositionSize.toString(),
+      isAdaptiveFeeEnabled: await configStorage.isAdaptiveFeeEnabledByMarketIndex(each.marketIndex),
+    };
+    const newOne = {
+      marketIndex: each.marketIndex,
+      assetId: ethers.utils.parseBytes32String(each.assetId),
+      increasePositionFeeRateBPS: each.increasePositionFeeRateBPS,
+      decreasePositionFeeRateBPS: each.decreasePositionFeeRateBPS,
+      initialMarginFractionBPS: each.initialMarginFractionBPS,
+      maintenanceMarginFractionBPS: each.maintenanceMarginFractionBPS,
+      maxProfitRateBPS: each.maxProfitRateBPS,
+      assetClass: each.assetClass,
+      allowIncreasePosition: each.allowIncreasePosition,
+      active: each.active,
+      fundingRate: {
+        maxSkewScaleUSD: each.fundingRate.maxSkewScaleUSD?.toString(),
+        maxFundingRate: each.fundingRate.maxFundingRate?.toString(),
+      },
+      maxLongPositionSize: each.maxLongPositionSize.toString(),
+      maxShortPositionSize: each.maxShortPositionSize.toString(),
+      isAdaptiveFeeEnabled: each.isAdaptiveFeeEnabled,
+    };
+    console.table({ existing, newOne });
+    const confirm = readlineSync.question(
+      `[configs/ConfigStorage] Confirm to update market index ${each.marketIndex}? (y/n): `
     );
-    const existingMarketConfig = await configStorage.marketConfigs(marketConfigs[i].marketIndex);
-    if (existingMarketConfig.assetId !== marketConfigs[i].assetId) {
-      console.log(`marketIndex ${marketConfigs[i].marketIndex} wrong asset id`);
+    switch (confirm) {
+      case "y":
+        break;
+      case "n":
+        console.log("[configs/ConfigStorage] Set Market Config cancelled!");
+        return;
+      default:
+        console.log("[configs/ConfigStorage] Invalid input!");
+        return;
+    }
+  }
+
+  console.log("[ConfigStorage] Setting market config...");
+  for (let i = 0; i < toBeMarketConfigs.length; i++) {
+    console.log(
+      `[ConfigStorage] Setting ${ethers.utils.parseBytes32String(toBeMarketConfigs[i].assetId)} market config...`
+    );
+    const existingMarketConfig = currentMarketConfigs[toBeMarketConfigs[i].marketIndex];
+    if (existingMarketConfig.assetId !== toBeMarketConfigs[i].assetId) {
+      console.log(`marketIndex ${toBeMarketConfigs[i].marketIndex} wrong asset id`);
       throw "bad asset id";
     }
-    // await safeWrapper.proposeTransaction(
-    //   tradeHelper.address,
-    //   0,
-    //   tradeHelper.interface.encodeFunctionData("updateBorrowingRate", [marketConfigs[i].assetClass])
-    // );
-    // await safeWrapper.proposeTransaction(
-    //   tradeHelper.address,
-    //   0,
-    //   tradeHelper.interface.encodeFunctionData("updateFundingRate", [marketConfigs[i].marketIndex])
-    // );
     const tx = await safeWrapper.proposeTransaction(
       configStorage.address,
       0,
       configStorage.interface.encodeFunctionData("setMarketConfig", [
-        marketConfigs[i].marketIndex,
-        marketConfigs[i],
-        marketConfigs[i].isAdaptiveFeeEnabled,
+        toBeMarketConfigs[i].marketIndex,
+        toBeMarketConfigs[i],
+        toBeMarketConfigs[i].isAdaptiveFeeEnabled,
       ])
     );
     console.log(`[ConfigStorage] Tx: ${tx}`);

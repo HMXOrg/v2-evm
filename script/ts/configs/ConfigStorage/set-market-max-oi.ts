062273b9ce608a6c88d5d9d8f679414121b5f972
Jonathan Xina <john.xina.p88@gmail.com>
2024-07-01 12:56:08 +0700

    feat: improve set max oi

--- script/ts/configs/ConfigStorage/set-market-max-oi.ts
+++ script/ts/configs/ConfigStorage/set-market-max-oi.ts
@@ -4,6 +4,7 @@ import { Command } from "commander";
 import signers from "../../entities/signers";
 import { OwnerWrapper } from "../../wrappers/OwnerWrapper";
 import { ethers } from "ethers";
+import * as readlineSync from "readline-sync";
 
 async function main(chainId: number) {
   const config = loadConfig(chainId);
@@ -21,17 +22,40 @@ async function main(chainId: number) {
   const ownerWrapper = new OwnerWrapper(chainId, deployer);
   const configStorage = ConfigStorage__factory.connect(config.storages.config, deployer);
 
-  console.log("[config/ConfigStorage] Set Market Max OIs...");
+  const currentMarketConfigs = await configStorage.getMarketConfigs();
+
+  console.log("[configs/ConfigStorage] Set Market Max OIs...");
   console.table(
     inputs.map((e) => {
+      const existingMaxLongPositionSize = Number(
+        ethers.utils.formatUnits(currentMarketConfigs[e.marketIndex].maxLongPositionSize, 30)
+      );
+      const existingMaxShortPositionSize = Number(
+        ethers.utils.formatUnits(currentMarketConfigs[e.marketIndex].maxShortPositionSize, 30)
+      );
       return {
         marketIndex: e.marketIndex,
         marketName: marketConfig.markets[e.marketIndex].name,
+        existingMaxLongPositionSize,
+        existingMaxShortPositionSize,
         maxLongPositionSize: e.maxLongPositionSize,
         maxShortPositionSize: e.maxShortPositionSize,
+        diffLong: `${(e.maxLongPositionSize / existingMaxLongPositionSize - 1) * 100}%`,
+        diffShort: `${(e.maxShortPositionSize / existingMaxShortPositionSize - 1) * 100}%`,
       };
     })
   );
+  const confirm = readlineSync.question(`[configs/ConfigStorage] Confirm to update Max OIs? (y/n): `);
+  switch (confirm) {
+    case "y":
+      break;
+    case "n":
+      console.log("[configs/ConfigStorage] Set Max OIs cancelled!");
+      return;
+    default:
+      console.log("[configs/ConfigStorage] Invalid input!");
+      return;
+  }
   await ownerWrapper.authExec(
     configStorage.address,
     configStorage.interface.encodeFunctionData("setMarketMaxOI", [
